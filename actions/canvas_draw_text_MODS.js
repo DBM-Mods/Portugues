88e7839c3e70290579e7fbe22982e053a2bbc99a
XinXyla <43226244+XinXyla@users.noreply.github.com>
2022-07-13 23:35:37 -0300

    Add files via upload

--- actions/canvas_draw_text_MODS.js
+++ actions/canvas_draw_text_MODS.js
@@ -14,31 +14,71 @@ subtitle: function(data) {
 	return `${data.text}`;
 },
 
-fields: ["storage", "varName", "x", "y", "fontPath", "fontColor", "fontSize", "align", "text"],
+fields: ["storage", "varName", "x", "y", "fontPath", "fontColor", "fontSize", "align", "text", "shadowcor", "blur", "shadowh", "shadowv", "fontColor2", "fontColor3",],
 
 html: function(isEvent, data) {
 	return `
-    <div style="position:absolute;bottom:0px;border: 1px solid #222;background:#000;color:#999;padding:3px;right:0px;z-index:999999">Versão 0.1</div>
+    <div style="position:absolute;bottom:0px;border: 1px solid #222;background:#000;color:#999;padding:3px;right:0px;z-index:999999">Versão 0.2</div>
     <div style="position:absolute;bottom:0px;border: 1px solid #222;background:#000;color:#999;padding:3px;left:0px;z-index:999999">dbmmods.com</div>
 
-	<table style="width:100%"><tr>
+	<div style="height: 370px; overflow-y: scroll;padding:0px 10px">
+
+	<table style="width:100%;"><tr>
 <td><span class="dbminputlabel">Imagem Canvas</span><br>
 <select id="storage" class="round" style="width: 100%" onchange="glob.refreshVariableList(this)">
 	${data.variables[1]}
 </select></td>
 <td><span class="dbminputlabel">Nome da Variavel</span><br>
 	<input id="varName" class="round" type="text" list="variableList">
-</td>
+</td></tr>
 
 <tr>
 <td>
 <span class="dbminputlabel">Fonte do Texto (Local/URL)</span><br>
 <input id="fontPath" class="round" type="text" value="fonts/">
 </td>
+<td style="width:175px">
+<span class="dbminputlabel">Tamanho</span><br>
+	<input id="fontSize" class="round" type="text" placeholder="Default size 10px">
+</td>
+</tr>
 
-<td>
-<span class="dbminputlabel">Cor do texto (Hex)</span><br>
-    <table style="width:100%"><tr><th><input value="#FFFFFF" id="fontColor" name="actionxinxyla" class="round" type="text" placeholder="Insira um código de cor HEX..."><th>
+</table>
+
+
+<div style="padding-top:8px">
+<span class="dbminputlabel">Sombra do Texto</span>
+<table style="width:100%;background:rgba(50,50,50,0.5);-webkit-border-radius: 10px;-moz-border-radius: 10px;border-radius: 10px;"><tr>
+<td style="width:30px"><span class="dbminputlabel">Borrão</span><br>
+<input id="blur" class="round" type="text" value="0"></td>
+
+<td style="width:100px"><span class="dbminputlabel">Mover X</span><br>
+<input id="shadowh" class="round" type="text" value="0"></td>
+<td style="width:100px"><span class="dbminputlabel">Mover Y</span><br>
+<input id="shadowv" class="round" type="text" value="0"></td>
+
+<td style="width:200px !important">
+<span class="dbminputlabel">Cor (HEX / RGBA)</span><br>
+    <table style="width:100%"><tr><th><input value="#FFFFFF" id="shadowcor" name="actionxinxyla" class="round" type="text" placeholder="Insira um código HEX ou RGBA..."><th>
+    <th style="width:40px;text-align:center;padding:4px"><a id="2btr1" style="cursor:pointer" onclick="(function(){
+      document.getElementById('shadowcor').type = 'color'
+      document.getElementById('2btr1').style.display = 'none';
+      document.getElementById('2btr2').style.display = 'block';
+      })()"><button class="tiny compact ui icon button">Cor</button></a><a id="2btr2" style="cursor:pointer;display:none" onclick="(function(){
+        document.getElementById('shadowcor').type = 'text';
+        document.getElementById('2btr1').style.display = 'block';
+        document.getElementById('2btr2').style.display = 'none';
+        })()"><button class="tiny compact ui icon button">Texto</button></a><th></tr></table>
+</td>
+
+</tr></table></div>
+
+<div style="padding-top:8px">
+<span class="dbminputlabel">Cor do Texto (HEX ou RGBA)</span>
+<table style="width:100%;background:rgba(50,50,50,0.5);-webkit-border-radius: 10px;-moz-border-radius: 10px;border-radius: 10px;"><tr>
+<td style="width:33% !important">
+<span class="dbminputlabel">Cor Principal</span><br>
+    <table style="width:100%"><tr><th><input value="#FFFFFF" id="fontColor" name="actionxinxyla" class="round" type="text" placeholder="Insira um código HEX ou RGBA..."><th>
     <th style="width:40px;text-align:center;padding:4px"><a id="btr1" style="cursor:pointer" onclick="(function(){
       document.getElementById('fontColor').type = 'color'
       document.getElementById('btr1').style.display = 'none';
@@ -50,8 +90,49 @@ html: function(isEvent, data) {
         })()"><button class="tiny compact ui icon button">Texto</button></a><th></tr></table>
 </td>
 
-<tr>
-<td>
+<td style="width:33% !important">
+<span class="dbminputlabel">Cor Diagonal 1</span><br>
+    <table style="width:100%"><tr><th><input id="fontColor2" name="actionxinxyla" class="round" type="text" placeholder="Insira um código HEX ou RGBA..."><th>
+    <th style="width:40px;text-align:center;padding:4px"><a id="3btr1" style="cursor:pointer" onclick="(function(){
+      document.getElementById('fontColor2').type = 'color'
+      document.getElementById('3btr1').style.display = 'none';
+      document.getElementById('3btr2').style.display = 'block';
+      })()"><button class="tiny compact ui icon button">Cor</button></a><a id="3btr2" style="cursor:pointer;display:none" onclick="(function(){
+        document.getElementById('fontColor2').type = 'text';
+        document.getElementById('3btr1').style.display = 'block';
+        document.getElementById('3btr2').style.display = 'none';
+        })()"><button class="tiny compact ui icon button">Texto</button></a><th></tr></table>
+</td>
+
+<td style="width:33% !important">
+<span class="dbminputlabel">Cor Diagonal 2</span><br>
+    <table style="width:100%"><tr><th><input id="fontColor3" name="actionxinxyla" class="round" type="text" placeholder="Insira um código HEX ou RGBA..."><th>
+    <th style="width:40px;text-align:center;padding:4px"><a id="4btr1" style="cursor:pointer" onclick="(function(){
+      document.getElementById('fontColor3').type = 'color'
+      document.getElementById('4btr1').style.display = 'none';
+      document.getElementById('4btr2').style.display = 'block';
+      })()"><button class="tiny compact ui icon button">Cor</button></a><a id="4btr2" style="cursor:pointer;display:none" onclick="(function(){
+        document.getElementById('fontColor3').type = 'text';
+        document.getElementById('4btr1').style.display = 'block';
+        document.getElementById('4btr2').style.display = 'none';
+        })()"><button class="tiny compact ui icon button">Texto</button></a><th></tr></table>
+</td>
+
+</tr></table></div>
+
+<div style="padding-top:8px">
+<span class="dbminputlabel">Posição do Texto</span>
+<table style="width:100%;background:rgba(50,50,50,0.5);-webkit-border-radius: 10px;-moz-border-radius: 10px;border-radius: 10px;"><tr>
+<td style="width:33% !important">
+<span class="dbminputlabel">Posição X</span><br>
+	<input id="x" class="round" type="text" value="0">
+</td>
+
+<td style="width:33% !important">
+<span class="dbminputlabel">Posição Y</span><br>
+	<input id="y" class="round" type="text" value="0">
+</td>
+<td style="width:33% !important">
 <span class="dbminputlabel">Alinhamento</span><br>
 	<select id="align" class="round" style="width: 100%;">
 	<option value="0" selected>Superior esquerdo</option>
@@ -66,31 +147,16 @@ html: function(isEvent, data) {
 	</select>
 </td>
 
-<td>
-<span class="dbminputlabel">Tamanho do texto</span><br>
-	<input id="fontSize" class="round" type="text" placeholder="Default size 10px">
-</td>
 
 
-<tr>
-<td>
-<span class="dbminputlabel">Posição X</span><br>
-	<input id="x" class="round" type="text" value="0">
-</td>
-
-<td>
-<span class="dbminputlabel">Posição Y</span><br>
-	<input id="y" class="round" type="text" value="0">
-</td>
-
-
-
-</tr></table>
+</tr></table></div>
 
 
 <br>
 <span class="dbminputlabel">Texto</span><br>
-	<textarea id="text" rows="4" placeholder="Insira seu texto aqui..." style="width: 100%; white-space: nowrap; resize: yes;"></textarea>
+	<textarea id="text" rows="3" placeholder="Insira seu texto aqui..." style="width: 100%; white-space: nowrap; resize: yes;"></textarea>
+</div>
+
 </div>
 
 <style>
@@ -121,6 +187,21 @@ action: function(cache) {
 	if (isNaN(fontSize)) {
 		fontSize = 10;
 	}
+	let blur = parseInt(this.evalMessage(data.blur, cache));
+	if (isNaN(blur)) {
+		blur = 0;
+	}
+	let shadowh = parseInt(this.evalMessage(data.shadowh, cache));
+	if (isNaN(shadowh)) {
+		shadowh = 0;
+	}
+	let shadowv = parseInt(this.evalMessage(data.shadowv, cache));
+	if (isNaN(shadowv)) {
+		shadowv = 0;
+	}
+	const shadowcor = this.evalMessage(data.shadowcor, cache);
+	const fontColor2 = this.evalMessage(data.fontColor2, cache);
+	const fontColor3 = this.evalMessage(data.fontColor3, cache);
 	const align = parseInt(data.align);
 	const x = parseInt(this.evalMessage(data.x, cache));
 	const y = parseInt(this.evalMessage(data.y, cache));
@@ -132,6 +213,10 @@ action: function(cache) {
 	ctx.drawImage(image, 0, 0, image.width, image.height);
 	Canvas.registerFont(fontPath, {family:fontName})
 	ctx.font = fontSize+"px "+fontName;
+	ctx.shadowColor = shadowcor
+	ctx.shadowOffsetX = shadowh;
+	ctx.shadowOffsetY = shadowv;
+	ctx.shadowBlur = blur;
 	switch(align) {
 		case 0:
 			ctx.textAlign = "left";
@@ -169,8 +254,16 @@ action: function(cache) {
 			ctx.textAlign = "right";
 			ctx.textBaseline = "bottom"; 
 	}
-	ctx.fillStyle = fontColor;
-	ctx.fillText(text, x, y);
+	if (fontColor2 !== "") {
+		ctx.fillStyle = fontColor2
+		ctx.fillText(text, x-(fontSize/15), y-(fontSize/15));
+	}
+	if (fontColor3 !== "") {
+		ctx.fillStyle = fontColor3
+		ctx.fillText(text, x+(fontSize/15), y+(fontSize/15));
+	}
+		ctx.fillStyle = fontColor;
+		ctx.fillText(text, x, y);
 	const result = canvas.toDataURL("image/png").replace("image/png", "image/octet-stream");
 	this.storeValue(result, storage, varName, cache);
 	this.callNextAction(cache);
