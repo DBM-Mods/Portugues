382a48699e4f211c9f23fc109fec5d3f1ac2ee84
XinXyla <43226244+XinXyla@users.noreply.github.com>
2022-07-30 01:19:32 -0300

    Add files via upload

---
+++ actions/find_webhook_MODS.js
@@ -0,0 +1,68 @@
+module.exports = {
+  name: 'Find Webhook MOD',
+  section: 'Webhook Control',
+  meta: {
+    version: '2.1.5',
+    preciseCheck: true,
+    author: '[XinXyla - 172782058396057602]',
+    authorUrl: 'https://github.com/DBM-Mods/Portugues',
+    downloadURL: 'https://github.com/DBM-Mods/Portugues/archive/refs/heads/main.zip',
+    },
+
+  subtitle(data) {
+    return `${data.varName}`;
+  },
+
+  variableStorage(data, varType) {
+    const type = parseInt(data.storage, 10);
+    if (type !== varType) return;
+    return [data.varName, 'Webhook'];
+  },
+
+  fields: ['url', 'storage', 'varName'],
+
+  html(isEvent, data) {
+    return `
+    <div style="position:absolute;bottom:0px;border: 1px solid #222;background:#000;color:#999;padding:3px;right:0px;z-index:999999">Versão 0.1</div>
+    <div style="position:absolute;bottom:0px;border: 1px solid #222;background:#000;color:#999;padding:3px;left:0px;z-index:999999">dbmmods.com</div>
+
+<div>
+  <span class="dbminputlabel">URL do Webhook</span><br>
+    <input id="url" class="round" type="text">
+ </div><br>
+<div style="padding-top: 8px;">
+  <div style="float: left; width: 35%;">
+  <span class="dbminputlabel">Armazenar em</span><br>
+    <select id="storage" class="round">
+      ${data.variables[1]}
+    </select>
+  </div>
+  <div id="varNameContainer" style="float: right; width: 60%;">
+  <span class="dbminputlabel">Nome da Variavel</span><br>
+    <input id="varName" class="round" type="text">
+  </div>
+</div>`;
+  },
+
+  init() {},
+
+  async action(cache) {
+    const { DiscordJS } = this.getDBM();
+    const data = cache.actions[cache.index];
+    const url = this.evalMessage(data.url, cache);
+    const result = new DiscordJS.WebhookClient({ url: url });
+
+    if (!result) {
+      console.log('Find Webhook MOD: Ocorreu um problema ao criar o objeto webhook.');
+      return this.callNextAction(cache);
+    }
+
+    const storage = parseInt(data.storage, 10);
+    const varName = this.evalMessage(data.varName, cache);
+    this.storeValue(result, storage, varName, cache);
+
+    this.callNextAction(cache);
+  },
+
+  mod() {},
+};
