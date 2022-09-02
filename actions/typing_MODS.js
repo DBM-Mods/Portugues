f472629de48e33f6001b573aa6ef4170b27cfc09
TempestDBM <48209160+TempestDBM@users.noreply.github.com>
2022-09-02 15:09:07 -0300

    Update typing_MODS.js

--- actions/typing_MODS.js
+++ actions/typing_MODS.js
@@ -2,7 +2,7 @@ module.exports = {
   name: 'Bot Typing MOD',
   section: 'Bot Client Control',
   meta: {
-    version: '2.1.5',
+    version: '2.1.6',
     preciseCheck: true,
     author: '[Tempest - 321400509326032897]',
     authorUrl: 'https://github.com/DBM-Mods/Portugues',
