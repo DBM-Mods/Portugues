9d463ce0b36d0c87fa666e70d798481a2dd3fe26
TempestDBM <48209160+TempestDBM@users.noreply.github.com>
2022-09-02 14:58:08 -0300

    Update go_to_anchor_MODS.js

--- actions/go_to_anchor_MODS.js
+++ actions/go_to_anchor_MODS.js
@@ -2,7 +2,7 @@ module.exports = {
   name: 'Go To Action Anchor MOD',
   section: 'Other Stuff',
   meta: {
-    version: '2.1.5',
+    version: '2.1.6',
     preciseCheck: true,
     author: '[XinXyla - 172782058396057602]',
     authorUrl: 'https://github.com/DBM-Mods/Portugues',
