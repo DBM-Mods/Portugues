a3d52a78e379f896da7949ccd7f55a75f2f8395e
TempestDBM <48209160+TempestDBM@users.noreply.github.com>
2022-09-21 15:58:19 -0300

    Add files via upload

---
+++ actions/check_if_emoji_exists_MODS.js
@@ -0,0 +1,181 @@
+module.exports = {
+  //---------------------------------------------------------------------
+  // Action Name
+  //
+  // This is the name of the action displayed in the editor.
+  //---------------------------------------------------------------------
+
+  name: "Check if Emoji Exists MOD",
+
+  //---------------------------------------------------------------------
+  // Action Section
+  //
+  // This is the section the action will fall into.
+  //---------------------------------------------------------------------
+
+  section: "Conditions",
+
+  //---------------------------------------------------------------------
+  // Action Subtitle
+  //
+  // This function generates the subtitle displayed next to the name.
+  //---------------------------------------------------------------------
+
+  subtitle(data, presets) {
+    return ``;
+  },
+
+  //---------------------------------------------------------------------
+  // Action Storage Function
+  //
+  // Stores the relevant variable info for the editor.
+  //---------------------------------------------------------------------
+
+  //---------------------------------------------------------------------
+  // Action Meta Data
+  //
+  // Helps check for updates and provides info if a custom mod.
+  // If this is a third-party mod, please set "author" and "authorUrl".
+  //
+  // It's highly recommended "preciseCheck" is set to false for third-party mods.
+  // This will make it so the patch version (0.0.X) is not checked.
+  //---------------------------------------------------------------------
+
+  meta: {
+    version: "2.1.6",
+    preciseCheck: true,
+    author: '[Tempest - 321400509326032897]',
+    authorUrl: 'https://github.com/DBM-Mods/Portugues',
+    downloadURL: 'https://github.com/DBM-Mods/Portugues/archive/refs/heads/main.zip',
+  },
+
+  //---------------------------------------------------------------------
+  // Action Fields
+  //
+  // These are the fields for the action. These fields are customized
+  // by creating elements with corresponding IDs in the HTML. These
+  // are also the names of the fields stored in the action's JSON data.
+  //---------------------------------------------------------------------
+
+  fields: ["info", "find", "mode", "branch", "server", "varName"],
+
+  //---------------------------------------------------------------------
+  // Command HTML
+  //
+  // This function returns a string containing the HTML used for
+  // editing actions.
+  //
+  // The "isEvent" parameter will be true if this action is being used
+  // for an event. Due to their nature, events lack certain information,
+  // so edit the HTML to reflect this.
+  //---------------------------------------------------------------------
+
+  html(isEvent, data) {
+    return `
+    <div style="position:absolute;bottom:0px;border: 1px solid #222;background:#000;color:#999;padding:3px;right:0px;z-index:999999">Versão 0.1</div>
+    <div style="position:absolute;bottom:0px;border: 1px solid #222;background:#000;color:#999;padding:3px;left:0px;z-index:999999">dbmmods.com</div>
+
+    <div id="divValue">
+      <server-input dropdownLabel="Servidor" selectId="server" variableContainerId="varNameContainer" variableInputId="varName"></server-input>
+      <br><br><br>
+    </div>
+
+    <dbm-checkbox id="mode" label="Procurar em todos os servidores" onchange="glob.change(this)"></dbm-checkbox>
+
+    <br>
+
+    <div>
+      <div style="float: left; width: 40%;">
+        <span class="dbminputlabel">Buscar por</span><br>
+        <select id="info" class="round">
+          <option value="0" selected>ID</option>
+          <option value="1">Nome</option>
+        </select>
+      </div>
+      <div style="float: right; width: 55%;">
+        <span class="dbminputlabel">Pesquisar por</span><br>
+        <input id="find" class="round" type="text">
+      </div>
+    </div>
+
+    <br><br><br>
+
+    <conditional-input id="branch" style="padding-top: 8px;"></conditional-input>`
+  },
+
+  //---------------------------------------------------------------------
+  // Action Editor Init Code
+  //
+  // When the HTML is first applied to the action editor, this code
+  // is also run. This helps add modifications or setup reactionary
+  // functions for the DOM elements.
+  //---------------------------------------------------------------------
+
+  init() {
+    glob.change = function (event) {
+      if(event.value) {
+        document.getElementById("divValue").style.display = "none";
+      } else {
+        document.getElementById("divValue").style.display = "block";
+      }
+    }
+
+    glob.change(document.getElementById("mode"));
+  },
+
+  //---------------------------------------------------------------------
+  // Action Bot Function
+  //
+  // This is the function for the action within the Bot's Action class.
+  // Keep in mind event calls won't have access to the "msg" parameter,
+  // so be sure to provide checks for variable existence.
+  //---------------------------------------------------------------------
+
+  async action(cache) {
+    const data = cache.actions[cache.index];
+    const find = this.evalMessage(data.find, cache);
+    const info = parseInt(data.info, 10);
+    let emojis;
+
+    if(data.mode) {
+      emojis = this.getDBM().Bot.bot.emojis.cache;
+    } else {
+      const server = await this.getServerFromData(data.server, data.varName, cache);
+
+      if (!server) {
+        this.callNextAction(cache);
+        return;
+      }
+
+      emojis = server.emojis.cache;
+    }
+
+    switch(info) {
+        case 0:
+          result = Boolean(emojis.get(find));
+          break;
+        case 1:
+          result = Boolean(emojis.find((c) => c.name === find));
+          break;
+    }
+
+
+    this.executeResults(result, data?.branch ?? data, cache);
+  },
+
+  //---------------------------------------------------------------------
+  // Action Bot Mod
+  //
+  // Upon initialization of the bot, this code is run. Using the bot's
+  // DBM namespace, one can add/modify existing functions if necessary.
+  // In order to reduce conflicts between mods, be sure to alias
+  // functions you wish to overwrite.
+  //---------------------------------------------------------------------
+
+  modInit(data) {
+    this.prepareActions(data.branch?.iftrueActions);
+    this.prepareActions(data.branch?.iffalseActions);
+  },
+
+  mod() {},
+};
