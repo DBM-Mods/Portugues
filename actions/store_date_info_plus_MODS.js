15c4428f5b4b3ee53bb7737e70bcaf34a84e2420
XinXyla <43226244+XinXyla@users.noreply.github.com>
2022-09-24 01:39:17 -0300

    Add files via upload

--- actions/store_date_info_plus_MODS.js
+++ actions/store_date_info_plus_MODS.js
@@ -14,7 +14,8 @@ module.exports = {
     subtitle: function(data) {
         const info = ['Dia da semana', 'Dia (número)', 'Dia do ano', 'Semana do ano', 'Mês do ano', 'Mês (número)', 'Ano', 'Hora', 'Minutos', 'Segundos', 'Milissegundos', 'Fuso horário', 'Unix Timestamp', 'Data completa']
         const storage = ['', 'Variável Temporária', 'Variável Servidor', 'Variável Global']
-        return `${data.modeStorage === "0" ? '"' + info[data.info] + '"' : data.buildInput === "" ? '"Não configurado"' : '"' + data.buildInput + '"'} de uma data ~ ${storage[data.storage]}`;
+        const formato = ['uma Data', 'um timestamp', 'um timestamp milissegundos']
+        return `${data.modeStorage === "0" ? '"' + info[data.info] + '"' : data.buildInput === "" ? '"Não configurado"' : '"' + data.buildInput + '"'} de ${formato[data.formato]||'uma data'} ~ ${storage[data.storage]}`;
     },
     
  
@@ -27,22 +28,29 @@ module.exports = {
         return ([data.varName, dataType]);
     },
         
-    fields: ["sourceDate", "dateLanguage", "modeStorage", "info", "buildInput", "storage", "varName"],
+    fields: ["formato", "timezone", "sourceDate", "dateLanguage", "modeStorage", "info", "buildInput", "storage", "varName"],
     
         html: function(isEvent, data) {
         return `
-        <div style="position:absolute;bottom:0px;border: 1px solid #222;background:#000;color:#999;padding:3px;right:0px;z-index:999999">Versão 0.3</div>
-    <div style="position:absolute;bottom:0px;border: 1px solid #222;background:#000;color:#999;padding:3px;left:0px;z-index:999999">dbmmods.com</div>
+        <div style="position:absolute;bottom:0px;border: 1px solid #222;background:#000;color:#999;padding:3px;right:0px;z-index:999999">Versão 0.4</div>
+        <div style="position:absolute;bottom:0px;border: 1px solid #222;background:#000;color:#999;padding:3px;left:0px;z-index:999999">dbmmods.com</div>
 
-        <div style="float: left; width: 62%;margin:0px 4px;">
-        <span class="dbminputlabel">Data de origem</span><br>
+        <span class="dbminputlabel">Formato</span><br>
+        <select id="formato" class="round" onchange="glob.onChangeMode2(this)">
+            <option value="0" selected>Data</option>
+            <option value="1">Timestamp</option>
+            <option value="2">Timestamp milissegundos</option>
+        </select>
+<br>
+        <div style="float: left; width: 62%;" id="manipulador">
+        <span class="dbminputlabel" name="xinelas">Data de origem</span><br>
             <input id="sourceDate" class="round" type="text" placeholder="Ex: Sun Oct 26 2019 10:38:01 GMT+0200">
         </div>
-        <div style="float: right; width: 35%">
+        <div style="float: right; width: 35%" id="dataxin">
         <span class="dbminputlabel">Idioma da data (iniciais)</span><br>
            <input id="dateLanguage" class="round" placeholder='O padrão é "en" (Inglês)'>
         </div><br><br><br>
-        <div style="float: left; width: 35%; margin:0px 4px">
+        <div style="float: left; width: 35%">
         <span class="dbminputlabel">Modo</span><br>
             <select id="modeStorage" class="round" onchange="glob.onChangeMode(this)">
                 <option value="0" selected>Selecionar</option>
@@ -69,10 +77,14 @@ module.exports = {
             </select>
         </div>
         <div id="buildMode" style="display: none; float: right; width: 62%">
-        <span class="dbminputlabel">Construir (<span class="wrexlink" data-url="https://momentjs.com/docs/#/displaying/format/">Documento</span>)</span><br>
+        <span class="dbminputlabel">Construir (<span class="xinxylalink" data-url="https://momentjs.com/docs/#/displaying/format/">Documento</span>)</span><br>
             <input id="buildInput" class="round" placeholder="Ex: DD/MM/YYYY [às] HH:mm:ss">
         </div><br><br><br>
-        <div style="float: left; width: 35%;margin:0px 4px">
+        <span class="dbminputlabel">Timezone (<span class="xinxylalink" data-url="https://en.wikipedia.org/wiki/List_of_tz_database_time_zones">Timezones</span>)</span><br>
+        <input id="timezone" class="round" type="text" placeholder="Ex: America/Sao_Paulo ou deixe vazio para o local">
+        <br>
+
+        <div style="float: left; width: 35%">
         <span class="dbminputlabel">Armazenar em</span><br>
             <select id="storage" class="round">
                 ${data.variables[1]}
@@ -87,13 +99,13 @@ module.exports = {
             <b>Ex:</b> <span id="code">DD/MM/YYYY [ás] HH:mm:ss</span> = <span id="code">30/01/2022 ás 13:38:20</span>
         </div>
         <style>
-             span.wrexlink {
+             span.xinxylalink {
 		color: #99b3ff;
 		text-decoration: underline;
                 cursor: pointer
             }
 
-	     span.wrexlink:hover { 
+	     span.xinxylalink:hover { 
                 color:#4676b9
             }
 
@@ -129,14 +141,35 @@ module.exports = {
 
         glob.onChangeMode(document.getElementById("modeStorage"));
 
-        var wrexlinks = document.getElementsByClassName("wrexlink")
-        for(var x = 0; x < wrexlinks.length; x++) {
+        glob.onChangeMode2 = function(event) {
+            const value = parseInt(event.value);
+            if (value == 0) {
+                document.querySelector("[name='xinelas']").innerText = (`Data de origem`);
+                document.getElementById("dataxin").style.display = null;
+                document.getElementById("manipulador").style.width = "62%";
+            }
+            if (value == 1) {
+                document.querySelector("[name='xinelas']").innerText = (`Timestamp`);
+                document.getElementById("dataxin").style.display = "none";
+                document.getElementById("manipulador").style.width = "100%";
+            }
+            if (value == 2) {
+                document.querySelector("[name='xinelas']").innerText = (`Timestamp milissegundos`);
+                document.getElementById("dataxin").style.display = "none";
+                document.getElementById("manipulador").style.width = "100%";
+            }
+            }
+
+        glob.onChangeMode2(document.getElementById("formato"));
+
+        var xinxylalinks = document.getElementsByClassName("xinxylalink")
+        for(var x = 0; x < xinxylalinks.length; x++) {
           
-          var wrexlink = wrexlinks[x];
-          var url = wrexlink.getAttribute('data-url');   
+          var xinxylalink = xinxylalinks[x];
+          var url = xinxylalink.getAttribute('data-url');   
           if (url) {
-            wrexlink.setAttribute("title", url);
-            wrexlink.addEventListener("click", function(e){
+            xinxylalink.setAttribute("title", url);
+            xinxylalink.addEventListener("click", function(e){
               e.stopImmediatePropagation();
               console.log("Launching URL: [" + url + "] in your default browser.")
               require('child_process').execSync('start ' + url);
@@ -149,9 +182,39 @@ module.exports = {
     
     action: function(cache) {
         const data = cache.actions[cache.index];
-        const moment = require("moment");
-        const dateLanguage = this.evalMessage(data.dateLanguage, cache);
-        const date = moment(Date.parse(this.evalMessage(data.sourceDate, cache)), "", dateLanguage === "" ? "en" : dateLanguage);
+        moment = require("moment");
+
+        timezone = this.evalMessage(data.timezone, cache);
+        timezonegat = "on"
+        if(timezone == "" || timezone == "undefined" || timezone == undefined){timezonegat = "off"}
+
+        if (data.formato == "0" || data.formato == undefined || data.formato == "undefined"){
+        dateLanguage = this.evalMessage(data.dateLanguage, cache);
+        date = moment(Date.parse(this.evalMessage(data.sourceDate, cache)), "", dateLanguage === "" ? "en" : dateLanguage)
+        
+        if(timezonegat == "on"){date.tz(timezone)}
+
+        }
+
+        if(data.formato == "1" || data.formato == "2"){
+
+        toDate = require('normalize-date')
+
+        if(data.formato == "2"){
+            datafonte = parseFloat(this.evalMessage(data.sourceDate, cache))
+            date = Math.floor(datafonte/1000)
+        } 
+        else { date = this.evalMessage(data.sourceDate, cache) }
+        
+        if (/^\d+(?:\.\d*)?$/.exec(date)) { unix = toDate((+date).toFixed(3)) }
+        else { unix = toDate(date) }
+
+         date = moment(unix)
+         if(timezonegat == "on"){date.tz(timezone)}
+
+        }
+
+
         const buildInput = this.evalMessage(data.buildInput, cache);
         const modeType = parseInt(this.evalMessage(data.modeStorage, cache));
         const info = parseInt(data.info);
