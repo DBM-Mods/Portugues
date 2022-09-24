2253f4baf08cea9669e449e4f3004a11b278edc1
XinXyla <43226244+XinXyla@users.noreply.github.com>
2022-09-24 03:51:09 -0300

    Add files via upload

--- actions/update_bot_MODS.js
+++ actions/update_bot_MODS.js
@@ -11,7 +11,9 @@
   
     subtitle(data, presets) {
       const atualizar = [
+        "",
         "Atualizar todos os comandos",
+        "",
         `Atualizar comando: ${data.alvo}`,
         "Atualizar comando atual",
       ]
@@ -28,7 +30,7 @@
 
     html: function(isEvent, data) {
       return `
-      <div style="position:absolute;bottom:0px;border: 1px solid #222;background:#000;color:#999;padding:3px;right:0px;z-index:999999">Versão 0.2</div>
+      <div style="position:absolute;bottom:0px;border: 1px solid #222;background:#000;color:#999;padding:3px;right:0px;z-index:999999">Versão 0.3</div>
       <div style="position:absolute;bottom:0px;border: 1px solid #222;background:#000;color:#999;padding:3px;left:0px;z-index:999999">dbmmods.com</div>
 
     <table style="width:100%"><tr><td>
