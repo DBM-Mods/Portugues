0b82a83582fb638a1674aa96664a0adadd8a4147
TempestDBM <48209160+TempestDBM@users.noreply.github.com>
2022-11-23 09:07:15 -0300

    Add files via upload

--- actions/store_interaction_info_MODS.js
+++ actions/store_interaction_info_MODS.js
@@ -4,7 +4,7 @@ module.exports = {
   meta: {
     version: '2.1.6',
     preciseCheck: true,
-    author: '[XinXyla - 172782058396057602]',
+    author: '[XinXyla - 172782058396057602]<br>[Tempest - 321400509326032897]',
     authorUrl: 'https://github.com/DBM-Mods/Portugues',
     downloadURL: 'https://github.com/DBM-Mods/Portugues/archive/refs/heads/main.zip',
   },
@@ -20,25 +20,27 @@ module.exports = {
       "ID do canal da interação",
       "Objeto > Opções da Interação",
       "Total de parâmetros",
+      "Mensagem da interação",
+      "ID da mensagem da interação",
     ];
     return `${info[parseInt(data.info, 10)]}`;
   },
 
 
-  variableStorage: function(data, varType) {
+  variableStorage: function (data, varType) {
     const type = parseInt(data.storage);
     const prse2 = parseInt(data.info);
-    const info2 = ['Object','ID', 'Language','Type','Token','Channel','Channel ID','Object > Options','Number'];
-    if(type !== varType) return;
+    const info2 = ['Objeto', 'ID', 'Linguagem', 'Tipo', 'Token', 'Canal', 'ID Canal', 'Objeto > Opções', 'Número', 'Mensagem', 'ID Mensagem'];
+    if (type !== varType) return;
     return ([data.varName2, info2[prse2]]);
-},
+  },
 
 
   fields: ["info", "storage", "varName2"],
 
   html(isEvent, data) {
     return `
-    <div style="position:absolute;bottom:0px;border: 1px solid #222;background:#000;color:#999;padding:3px;right:0px;z-index:999999">Versão 0.2</div>
+    <div style="position:absolute;bottom:0px;border: 1px solid #222;background:#000;color:#999;padding:3px;right:0px;z-index:999999">Versão 0.3</div>
     <div style="position:absolute;bottom:0px;border: 1px solid #222;background:#000;color:#999;padding:3px;left:0px;z-index:999999">dbmmods.com</div>
 
     <div>
@@ -50,6 +52,8 @@ module.exports = {
   <option value="2">Linguagem do autor</option>
   <option value="3">Tipo de interação</option>
   <option value="4">Token da interação</option>
+  <option value="9">Mensagem da interação</option>
+  <option value="10">ID da mensagem da interação</option>
   <option value="5">Canal da interação</option>
   <option value="6">ID do canal da interação</option>
   <option value="8">Total de parâmetros</option>
@@ -71,12 +75,11 @@ module.exports = {
 	</div>`;
   },
 
-  init: function() {
-    const {glob, document} = this;
-  
+  init: function () {
+    const { glob, document } = this;
+
     glob.variableChange(document.getElementById('storage'), 'varNameContainer');
   },
-
 
   async action(cache) {
     const data = cache.actions[cache.index];
@@ -97,25 +100,32 @@ module.exports = {
       case 3:
         result = interaction.type;
         break;
-        case 4:
+      case 4:
         result = interaction.token;
         break;
-        case 5:
+      case 5:
         result = interaction.channel;
         break;
-        case 6:
-          result = interaction.channel.id;
-          break;
-        case 7:
+      case 6:
+        result = interaction.channel.id;
+        break;
+      case 7:
         result = interaction.options._hoistedOptions;
         break;
-        case 8:
-          parametros = interaction.options._hoistedOptions
-          result = parametros.length
-          break;
+      case 8:
+        parametros = interaction.options._hoistedOptions
+        result = parametros.length
+        break;
+      case 9:
+        result = interaction.message;
+        break;
+      case 10:
+        result = interaction.message.id;
+        break;
       default:
         break;
     }
+
     if (result !== undefined) {
       const storage = parseInt(data.storage, 10);
       const varName2 = this.evalMessage(data.varName2, cache);
@@ -124,5 +134,5 @@ module.exports = {
     this.callNextAction(cache);
   },
 
-  mod() {},
+  mod() { },
 };
