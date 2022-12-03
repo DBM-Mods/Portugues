bdc2ffea8cc0b4ec821d37ae05a825072c019e00
XinXyla <43226244+XinXyla@users.noreply.github.com>
2022-12-03 11:45:39 -0300

    🆙 Atualização para a versão 0.2

--- actions/add_member_to_thread_MODS.js
+++ actions/add_member_to_thread_MODS.js
@@ -1,74 +1,46 @@
 module.exports = {
-    //---------------------------------------------------------------------
-    // Action Name
-    //
-    // This is the name of the action displayed in the editor.
-    //---------------------------------------------------------------------
-
     name: "Add Member to Thread MOD",
-
-    //---------------------------------------------------------------------
-    // Action Section
-    //
-    // This is the section the action will fall into.
-    //---------------------------------------------------------------------
-
     section: "Channel Control",
-
-    //---------------------------------------------------------------------
-    // Action Subtitle
-    //
-    // This function generates the subtitle displayed next to the name.
-    //---------------------------------------------------------------------
-
-    subtitle(data, presets) {
-        const storeTypes = presets.variables;
-        return `${storeTypes[parseInt(data.storage, 10)]} (${data.varName})`;
-    },
-
-    //---------------------------------------------------------------------
-    // Action Meta Data
-    //
-    // Helps check for updates and provides info if a custom mod.
-    // If this is a third-party mod, please set "author" and "authorUrl".
-    //
-    // It's highly recommended "preciseCheck" is set to false for third-party mods.
-    // This will make it so the patch version (0.0.X) is not checked.
-    //---------------------------------------------------------------------
-
     meta: {
         version: '2.1.6',
         preciseCheck: true,
-        author: '[Tempest - 321400509326032897]',
+        author: '[Tempest - 321400509326032897]<br>[XinXyla - 172782058396057602]',
         authorUrl: 'https://github.com/DBM-Mods/Portugues',
         downloadURL: 'https://github.com/DBM-Mods/Portugues/archive/refs/heads/main.zip',
     },
 
-    //---------------------------------------------------------------------
-    // Action Fields
-    //
-    // These are the fields for the action. These fields are customized
-    // by creating elements with corresponding IDs in the HTML. These
-    // are also the names of the fields stored in the action's JSON data.
-    //---------------------------------------------------------------------
+    subtitle(data, presets) {
 
-    fields: ["storage", "varName", "member", "varName2", "iffalse", "iffalseVal"],
+        if(data.descriptionx == true){
+          desccor = data.descriptioncolor
+          } else {
+            desccor = 'none'
+          }
+    
+        const storeTypes = presets.variables;
 
-    //---------------------------------------------------------------------
-    // Command HTML
-    //
-    // This function returns a string containing the HTML used for
-    // editing actions.
-    //
-    // The "isEvent" parameter will be true if this action is being used
-    // for an event. Due to their nature, events lack certain information,
-    // so edit the HTML to reflect this.
-    //---------------------------------------------------------------------
+        return data.description
+        ? `<font style="color:${desccor}">${data.description}</font>`
+        : `<font style="color:${desccor}">${storeTypes[parseInt(data.storage, 10)]} (${data.varName})</font>`
+      },
+
+    fields: ["storage", "varName", "member", "varName2", "iffalse", "iffalseVal","descriptioncolor","description","descriptionx"],
+
 
     html(isEvent, data) {
         return `
-      <div style="position:absolute;bottom:0px;border: 1px solid #222;background:#000;color:#999;padding:3px;right:0px;z-index:999999">Versão 0.1</div>
-      <div style="position:absolute;bottom:0px;border: 1px solid #222;background:#000;color:#999;padding:3px;left:0px;z-index:999999">dbmmods.com</div>
+        <div class="dbmmodsbr1 xinelaslink" data-url="https://github.com/DBM-Mods/Portugues/archive/refs/heads/main.zip">Atualizar</div>
+        <div class="dbmmodsbr2 xinelaslink" data-url="https://github.com/DBM-Mods/Portugues">Versão 0.2</div>
+
+        <div style="width: 100%; padding:5px 5px;height: calc(100vh - 160px);overflow:auto">
+
+    
+        <div id="flutuador" style="padding:0px 0px 15px 0px">
+    <table style="width:100%;"><tr>
+    <td><span class="dbminputlabel">Descrição da Action</span><br><input type="text" class="round" id="description" placeholder="Deixe vazio para remover"></td>
+    <td style="padding:0px 0px 0px 10px;width:70px"><div style="float:left;padding:0px 0px 0px 7px;margin-top:-5px"><dbm-checkbox id="descriptionx" label="Cor"></dbm-checkbox></div><br><input type="color" value="#ffffff" class="round" id="descriptioncolor"></td>
+    </tr></table>
+    </div>
 
       <retrieve-from-variable dropdownLabel="Thread" selectId="storage" variableContainerId="varNameContainer" variableInputId="varName"></retrieve-from-variable>
       
@@ -94,16 +66,17 @@ module.exports = {
         <br>
         <input id="iffalseVal" class="round" name="actionxinxyla" type="text">
      </div>
+     </div>
+
+<style>
+
+.dbmmodsbr1{position:absolute;bottom:0px;border: 0px solid rgba(50,50,50,0.7);background:rgba(0,0,0,0.7);color:#999;padding:5px;left:0px;z-index:999999;cursor:pointer}
+.dbmmodsbr2{position:absolute;bottom:0px;border: 0px solid rgba(50,50,50,0.7);background:rgba(0,0,0,0.7);color:#999;padding:5px;right:0px;z-index:999999;cursor:pointer}
+
+</style>
     `;
     },
 
-    //---------------------------------------------------------------------
-    // Action Editor Init Code
-    //
-    // When the HTML is first applied to the action editor, this code
-    // is also run. This helps add modifications or setup reactionary
-    // functions for the DOM elements.
-    //---------------------------------------------------------------------
 
     init() {
         glob.onComparisonChanged = function (event) {
@@ -127,15 +100,22 @@ module.exports = {
         }
       
         glob.onComparisonChanged(document.getElementById("iffalse"));
-    },
 
-    //---------------------------------------------------------------------
-    // Action Bot Function
-    //
-    // This is the function for the action within the Bot's Action class.
-    // Keep in mind event calls won't have access to the "msg" parameter,
-    // so be sure to provide checks for variable existence.
-    //---------------------------------------------------------------------
+        const xinelaslinks = document.getElementsByClassName('xinelaslink');
+        for (let x = 0; x < xinelaslinks.length; x++) {
+          const xinelaslink = xinelaslinks[x];
+          const url = xinelaslink.getAttribute('data-url');
+          if (url) {
+           xinelaslink.setAttribute('title', url);
+           xinelaslink.addEventListener('click', (e) => {
+              e.stopImmediatePropagation();
+              console.log(`Launching URL: [${url}] in your default browser.`);
+              require('child_process').execSync(`start ${url}`);
+            });
+          }
+        }
+
+    },
 
     async action(cache) {
         const data = cache.actions[cache.index];
@@ -151,15 +131,6 @@ module.exports = {
             this.executeResults(false, data, cache);
         }
     },
-
-    //---------------------------------------------------------------------
-    // Action Bot Mod
-    //
-    // Upon initialization of the bot, this code is run. Using the bot's
-    // DBM namespace, one can add/modify existing functions if necessary.
-    // In order to reduce conflicts between mods, be sure to alias
-    // functions you wish to overwrite.
-    //---------------------------------------------------------------------
 
     mod() { },
 };
