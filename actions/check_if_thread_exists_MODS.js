5cc7ae767edeb12fda67f7ec60e65d518bf4650f
XinXyla <43226244+XinXyla@users.noreply.github.com>
2022-12-05 13:21:14 -0300

    🆙 Atualização para a versão 0.2

--- actions/check_if_thread_exists_MODS.js
+++ actions/check_if_thread_exists_MODS.js
@@ -1,121 +1,111 @@
-module.exports = {
-    //---------------------------------------------------------------------
-    // Action Name
-    //
-    // This is the name of the action displayed in the editor.
-    //---------------------------------------------------------------------
-  
-    name: "Check if Thread Exists MOD",
-  
-    //---------------------------------------------------------------------
-    // Action Section
-    //
-    // This is the section the action will fall into.
-    //---------------------------------------------------------------------
-  
+module.exports = { 
+    name: "Check if Thread Exists MOD",  
     section: "Conditions",
-  
-    //---------------------------------------------------------------------
-    // Action Subtitle
-    //
-    // This function generates the subtitle displayed next to the name.
-    //---------------------------------------------------------------------
-  
-    subtitle(data, presets) {
-      return ``;
-    },
-  
-    //---------------------------------------------------------------------
-    // Action Storage Function
-    //
-    // Stores the relevant variable info for the editor.
-    //---------------------------------------------------------------------
-  
-    //---------------------------------------------------------------------
-    // Action Meta Data
-    //
-    // Helps check for updates and provides info if a custom mod.
-    // If this is a third-party mod, please set "author" and "authorUrl".
-    //
-    // It's highly recommended "preciseCheck" is set to false for third-party mods.
-    // This will make it so the patch version (0.0.X) is not checked.
-    //---------------------------------------------------------------------
-  
     meta: {
       version: "2.1.6",
       preciseCheck: true,
-      author: '[Tempest - 321400509326032897]',
+      author: '[Tempest - 321400509326032897]<br>[XinXyla - 172782058396057602]',
       authorUrl: 'https://github.com/DBM-Mods/Portugues',
       downloadURL: 'https://github.com/DBM-Mods/Portugues/archive/refs/heads/main.zip',
     },
   
-    //---------------------------------------------------------------------
-    // Action Fields
-    //
-    // These are the fields for the action. These fields are customized
-    // by creating elements with corresponding IDs in the HTML. These
-    // are also the names of the fields stored in the action's JSON data.
-    //---------------------------------------------------------------------
+    subtitle(data, presets) {
+
+      if(data.descriptionx == true){
+        desccor = data.descriptioncolor
+        } else {
+          desccor = 'none'
+        }
   
-    fields: ["channel", "varName", "info", "find", "branch"],
+      const storage = presets.variables;
   
-    //---------------------------------------------------------------------
-    // Command HTML
-    //
-    // This function returns a string containing the HTML used for
-    // editing actions.
-    //
-    // The "isEvent" parameter will be true if this action is being used
-    // for an event. Due to their nature, events lack certain information,
-    // so edit the HTML to reflect this.
-    //---------------------------------------------------------------------
+      return data.description
+      ? `<font style="color:${desccor}">${data.description}</font>`
+      : `<font style="color:${desccor}">${storage[parseInt(data.storage, 10)]} (${data.varName2})</font>`
+    },
+   
+    fields: ["channel", "varName", "info", "find", "branch", "storage", "varName2","descriptioncolor","description","descriptionx"],
+
+    variableStorage(data, varType) {
+      const type = parseInt(data.storage, 10);
+      if (type !== varType) return;
+      return [data.varName2, "Canal"];
+    },
+  
   
     html(isEvent, data) {
       return `
-      <div style="position:absolute;bottom:0px;border: 1px solid #222;background:#000;color:#999;padding:3px;right:0px;z-index:999999">Versão 0.1</div>
-      <div style="position:absolute;bottom:0px;border: 1px solid #222;background:#000;color:#999;padding:3px;left:0px;z-index:999999">dbmmods.com</div>
+      <div class="dbmmodsbr1 xinelaslink" data-url="https://github.com/DBM-Mods/Portugues/archive/refs/heads/main.zip">Atualizar</div>
+      <div class="dbmmodsbr2 xinelaslink" data-url="https://github.com/DBM-Mods/Portugues">Versão 0.2</div>
+  
+      <div style="width: 100%; padding:5px 5px;height: calc(100vh - 160px);overflow:auto">
+  
+      <div id="flutuador" style="padding:0px 0px 15px 0px">
+  <table style="width:100%;"><tr>
+  <td><span class="dbminputlabel">Descrição da Action</span><br><input type="text" class="round" id="description" placeholder="Deixe vazio para remover"></td>
+  <td style="padding:0px 0px 0px 10px;width:70px"><div style="float:left;padding:0px 0px 0px 7px;margin-top:-5px"><dbm-checkbox id="descriptionx" label="Cor"></dbm-checkbox></div><br><input type="color" value="#ffffff" class="round" id="descriptioncolor"></td>
+  </tr></table>
+  </div>
   
       <channel-input dropdownLabel="Canal" selectId="channel" variableContainerId="varNameContainer" variableInputId="varName"></channel-input>
   
       <br><br><br>
   
-      <div>
-        <div style="float: left; width: 40%;">
+      <div style="padding:8px 0px;">
+        <div style="float: left; width: 35%;">
           <span class="dbminputlabel">Buscar por</span><br>
           <select id="info" class="round">
             <option value="0" selected>ID</option>
             <option value="1">Nome</option>
           </select>
         </div>
-        <div style="float: right; width: 55%;">
+        <div style="float: right; width: 60%;">
           <span class="dbminputlabel">Pesquisar por</span><br>
           <input id="find" class="round" type="text">
         </div>
       </div>
   
-      <br><br><br><br>
-  
-      <conditional-input id="branch" style="padding-top: 8px;"></conditional-input>`
+      <br><br>
+      <div style="padding:8px 0px;">
+      <conditional-input id="branch" style="padding-top: 8px;"></conditional-input>
+      </div>
+      <br><br>
+      <div style="padding:8px 0px;">
+      <store-in-variable allowNone style="padding-top: 8px;" selectId="storage" variableInputId="varName2" variableContainerId="varNameContainer2"></store-in-variable>
+      </div>
+
+      </div>
+
+            <style>
+            table{width:100%}
+            .col1{width:32%}
+            .col2{width:60%;padding:0px 0px 0px 20px}
+.dbmmodsbr1{position:absolute;bottom:0px;border: 0px solid rgba(50,50,50,0.7);background:rgba(0,0,0,0.7);color:#999;padding:5px;left:0px;z-index:999999;cursor:pointer}
+.dbmmodsbr2{position:absolute;bottom:0px;border: 0px solid rgba(50,50,50,0.7);background:rgba(0,0,0,0.7);color:#999;padding:5px;right:0px;z-index:999999;cursor:pointer}
+
+</style>
+      
+      `
     },
-  
-    //---------------------------------------------------------------------
-    // Action Editor Init Code
-    //
-    // When the HTML is first applied to the action editor, this code
-    // is also run. This helps add modifications or setup reactionary
-    // functions for the DOM elements.
-    //---------------------------------------------------------------------
-  
-    init() {},
-  
-    //---------------------------------------------------------------------
-    // Action Bot Function
-    //
-    // This is the function for the action within the Bot's Action class.
-    // Keep in mind event calls won't have access to the "msg" parameter,
-    // so be sure to provide checks for variable existence.
-    //---------------------------------------------------------------------
-  
+   
+    init() {
+      const { glob, document } = this;
+
+      const xinelaslinks = document.getElementsByClassName('xinelaslink');
+      for (let x = 0; x < xinelaslinks.length; x++) {
+        const xinelaslink = xinelaslinks[x];
+        const url = xinelaslink.getAttribute('data-url');
+        if (url) {
+         xinelaslink.setAttribute('title', url);
+         xinelaslink.addEventListener('click', (e) => {
+            e.stopImmediatePropagation();
+            console.log(`Launching URL: [${url}] in your default browser.`);
+            require('child_process').execSync(`start ${url}`);
+          });
+        }
+      }
+    },
+   
     async action(cache) {
       const data = cache.actions[cache.index];
       const channel = await this.getChannelFromData(data.channel, data.varName, cache);
@@ -131,25 +121,22 @@ module.exports = {
   
       switch(info) {
             case 0:
-                result = Boolean(threads.get(find));
+                result = threads.get(find);
                 break;
             case 1:
-                result = Boolean(threads.find((c) => c.name === find));
+                result = threads.find((c) => c.name === find);
                 break;
       }
   
-      this.executeResults(result, data?.branch ?? data, cache);
+      var result2 = Boolean(result)
+    if (result !== undefined) {
+      const storage = parseInt(data.storage, 10);
+      const varName2 = this.evalMessage(data.varName2, cache);
+      this.storeValue(result, storage, varName2, cache);
+    }
+      this.executeResults(result2, data?.branch ?? data, cache);
     },
-  
-    //---------------------------------------------------------------------
-    // Action Bot Mod
-    //
-    // Upon initialization of the bot, this code is run. Using the bot's
-    // DBM namespace, one can add/modify existing functions if necessary.
-    // In order to reduce conflicts between mods, be sure to alias
-    // functions you wish to overwrite.
-    //---------------------------------------------------------------------
-  
+    
     modInit(data) {
       this.prepareActions(data.branch?.iftrueActions);
       this.prepareActions(data.branch?.iffalseActions);
