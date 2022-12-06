66a4f3931a904715ffc4b11c62476f99ae3e0dd2
TempestDBM <48209160+TempestDBM@users.noreply.github.com>
2022-12-06 13:02:50 -0300

    Add files via upload

--- actions/leave_server_MODS.js
+++ actions/leave_server_MODS.js
@@ -22,7 +22,16 @@ module.exports = {
   //---------------------------------------------------------------------
 
   subtitle(data, presets) {
-    return "";
+
+    if (data.descriptionx) {
+      desccor = data.descriptioncolor;
+    } else {
+      desccor = "none";
+    }
+
+    return data.description
+      ? `<font style="color:${desccor}">${data.description}</font>`
+      : `<font style="color:${desccor}">${presets.getServerText(data.server, data.varName)}</font>`
   },
 
   //---------------------------------------------------------------------
@@ -51,7 +60,7 @@ module.exports = {
   // are also the names of the fields stored in the action's JSON data.
   //---------------------------------------------------------------------
 
-  fields: ["server", "varName"],
+  fields: ["server", "varName", "description", "descriptionx", "descriptioncolor", "iffalse", "iffalseVal"],
 
   //---------------------------------------------------------------------
   // Command HTML
@@ -66,9 +75,26 @@ module.exports = {
 
   html(isEvent, data) {
     return `
-        <div style="position:absolute;bottom:0px;border: 1px solid #222;background:#000;color:#999;padding:3px;right:0px;z-index:999999">Versão 0.1</div>
-        <div style="position:absolute;bottom:0px;border: 1px solid #222;background:#000;color:#999;padding:3px;left:0px;z-index:999999">dbmmods.com</div>
-      
+        <div class="dbmmodsbr1 xinelaslink" data-url="https://github.com/DBM-Mods/Portugues/archive/refs/heads/main.zip">Atualizar</div>
+        <div class="dbmmodsbr2 xinelaslink" data-url="https://github.com/DBM-Mods/Portugues">Versão 0.2</div>
+
+        <div id="flutuador" style="padding:0px 0px 15px 0px">
+          <table style="width:100%;"><tr>
+            <td>
+              <span class="dbminputlabel">Descrição da Action</span>
+              <br>
+              <input type="text" class="round" id="description" placeholder="Deixe vazio para remover">
+            </td>
+            <td style="padding:0px 0px 0px 10px;width:70px">
+              <div style="float:left;padding:0px 0px 0px 7px;margin-top:-5px">
+                <dbm-checkbox id="descriptionx" label="Cor"></dbm-checkbox>
+              </div>
+              <br>
+              <input type="color" value="#ffffff" class="round" id="descriptioncolor">
+            </td>
+          </table>
+        </div>
+
         <server-input dropdownLabel="Servidor" selectId="server" variableContainerId="varNameContainer" variableInputId="varName"></server-input>
     
         <br><br><br>
@@ -77,6 +103,57 @@ module.exports = {
           <b>NOTA</b>
           <p>Essa action irá fazer seu bot sair do servidor</p>
         </div>
+
+        <br>
+
+        <div style="float: left; width: 40%">
+          <span class="dbminputlabel">Caso falhe</span>
+          <select id="iffalse" class="round" onchange="glob.onComparisonChanged(this)">
+              <option value="0" selecionado>Continuar ações</option>
+              <option value="1">Parar sequência de ação</option>
+              <option value="2">Ir para a ação</option>
+              <option value="3">Pular as próximas ações</option>
+              <option value="4">Ir para a âncora de ação</option>
+          </select>
+        </div>
+
+        <div id="iffalseContainer" style="display: none; float: right; width: 55%;">
+          <span id="xinelas" class="dbminputlabel">Para</span>
+          <br>
+          <input id="iffalseVal" class="round" name="actionxinxyla" type="text">
+        </div>
+
+        <style>
+          .xin {
+            padding: 5px;
+            border: 1px solid #777;
+            background: rgba(255,255,255,0.1);
+          }
+
+          .dbmmodsbr1 {
+            position: absolute;
+            bottom: 0px;
+            border: 0px solid rgba(50,50,50,0.7);
+            background: rgba(0,0,0,0.7);
+            color: #999;
+            padding: 5px;
+            left: 0px;
+            z-index: 999999;
+            cursor: pointer
+          }
+
+          .dbmmodsbr2 {
+            position: absolute;
+            bottom: 0px;
+            border: 0px solid rgba(50,50,50,0.7);
+            background: rgba(0,0,0,0.7);
+            color: #999;
+            padding: 5px;
+            right: 0px;
+            z-index: 999999;
+            cursor: pointer
+          }
+        </style>
     `;
   },
 
@@ -88,7 +165,43 @@ module.exports = {
   // functions for the DOM elements.
   //---------------------------------------------------------------------
 
-  init() {},
+  init() {
+    const xinelaslinks = document.getElementsByClassName("xinelaslink");
+    for (let x = 0; x < xinelaslinks.length; x++) {
+      const xinelaslink = xinelaslinks[x];
+      const url = xinelaslink.getAttribute('data-url');
+      if (url) {
+        xinelaslink.setAttribute('title', url);
+        xinelaslink.addEventListener('click', (e) => {
+          e.stopImmediatePropagation();
+          console.log(`Launching URL: [${url}] in your default browser.`);
+          require('child_process').execSync(`start ${url}`);
+        });
+      }
+    }
+
+    glob.onComparisonChanged = function (event) {
+      if (event.value > "1") {
+        document.getElementById("iffalseContainer").style.display = null;
+      } else {
+        document.getElementById("iffalseContainer").style.display = "none";
+      }
+
+      if (event.value == "2") {
+        document.querySelector("[id='xinelas']").innerText = (`Número da ação`);
+      }
+
+      if (event.value == "3") {
+        document.querySelector("[id='xinelas']").innerText = (`Pular ações`);
+      }
+
+      if (event.value == "4") {
+        document.querySelector("[id='xinelas']").innerText = (`Nome da âncora`);
+      }
+    }
+
+    glob.onComparisonChanged(document.getElementById("iffalse"));
+  },
 
   //---------------------------------------------------------------------
   // Action Bot Function
@@ -99,9 +212,15 @@ module.exports = {
   //---------------------------------------------------------------------
 
   async action(cache) {
+    const data = cache.actions[cache.index];
     const server = await this.getServerFromData(data.server, data.varName, cache);
-    server.leave();
-    this.callNextAction(cache);
+
+    try {
+      await server.leave();
+      this.callNextAction(cache);
+    } catch {
+      this.executeResults(false, data, cache);
+    }
   },
 
   //---------------------------------------------------------------------
@@ -113,5 +232,5 @@ module.exports = {
   // functions you wish to overwrite.
   //---------------------------------------------------------------------
 
-  mod() {},
+  mod() { },
 };
