01255c8e6bf47cdd8389cb2ea25b27bbec27c468
XinXyla <43226244+XinXyla@users.noreply.github.com>
2022-12-25 15:08:58 -0300

    🆙 Atualização para a versão 0.2

--- actions/file_control_MODS.js
+++ actions/file_control_MODS.js
@@ -2,23 +2,52 @@ module.exports = {
     name: 'File Control MOD',
     section: 'File Stuff',
     meta: {
-      version: '2.1.6',
-      preciseCheck: true,
-      author: '[Tempest - 321400509326032897]<br>[XinXyla - 172782058396057602]',
-      authorUrl: 'https://github.com/DBM-Mods/Portugues',
-      downloadURL: 'https://github.com/DBM-Mods/Portugues/archive/refs/heads/main.zip',
+        version: '2.1.6',
+        preciseCheck: true,
+        author: '[Tempest - 321400509326032897]<br>[XinXyla - 172782058396057602]',
+        authorUrl: 'https://github.com/DBM-Mods/Portugues',
+        downloadURL: 'https://github.com/DBM-Mods/Portugues/archive/refs/heads/main.zip',
     },
 
-    subtitle: function(data) {
-      return ``;
-    },
-  
-    fields: ['path', 'nome', 'format', 'function', 'iffalse', 'iffalseVal', 'text', 'copy', 'nome2', 'line'],
-  
+    subtitle(data, presets) {
+
+        const infoc = [
+            ``,
+            `Criar / Escrever`,
+            `Adicionar`,
+            `Inserir`,
+            `Deletar`,
+            `Copiar para ${data.copy}${data.nome2}`,
+            `Mover para ${data.copy}`,
+            `Renomear`,
+          ];
+
+        if(data.descriptionx == true){
+          desccor = data.descriptioncolor
+          } else {
+            desccor = 'none'
+          }
+    
+        return data.description
+        ? `<font style="color:${desccor}">${data.description}</font>`
+        : `<font style="color:${desccor}">${data.path}${data.nome}.${data.format} (${infoc[parseInt(data.function, 10)]})</font>`
+      },
+
+    fields: ['path', 'nome', 'format', 'function', 'iffalse', 'iffalseVal', 'text', 'texteval', 'addType2', 'copy', 'nome2', 'line', 'descriptioncolor', 'description', 'descriptionx'],
+
     html(_isEvent, data) {
-      return `
-        <div style="position:absolute;bottom:0px;border: 1px solid #222;background:#000;color:#999;padding:3px;right:0px;z-index:999999">Versão 0.1</div>
-        <div style="position:absolute;bottom:0px;border: 1px solid #222;background:#000;color:#999;padding:3px;left:0px;z-index:999999">dbmmods.com</div>
+        return `
+        <div class="dbmmodsbr1 xinelaslink" data-url="https://github.com/DBM-Mods/Portugues/archive/refs/heads/main.zip">Atualizar</div>
+    <div class="dbmmodsbr2 xinelaslink" data-url="https://github.com/DBM-Mods/Portugues">Versão 0.2</div>
+
+    <div style="width: 100%; padding:5px 5px;height: calc(100vh - 160px);overflow:auto">
+
+    <div id="flutuador" style="padding:0px 0px 0px 0px">
+<table style="width:100%;"><tr>
+<td><span class="dbminputlabel">Descrição da Action</span><br><input type="text" class="round" id="description" placeholder="Deixe vazio para remover"></td>
+<td style="padding:0px 0px 0px 10px;width:70px"><div style="float:left;padding:0px 0px 0px 7px;margin-top:-5px"><dbm-checkbox id="descriptionx" label="Cor"></dbm-checkbox></div><br><input type="color" value="#ffffff" class="round" id="descriptioncolor"></td>
+</tr></table>
+</div>
     
         <table style="width:100%"><tr>
         <td style="width:calc(50% - 50px)">
@@ -37,6 +66,7 @@ module.exports = {
         </tr></table>
 
         <xinspace>
+        
 <div style="float:left;width:calc(50% - 10px)" id="xinelasx">
         <span class="dbminputlabel">Função</span>
         <select id="function" class="round" onchange="glob.change(this)">
@@ -48,15 +78,18 @@ module.exports = {
             <option value="6">Mover</option>
             <option value="7">Renomear</option>
         </select></div>
+        
         <div class="campo1 campo2" style="float:right;width:calc(50% - 10px)">
             <span class="dbminputlabel">Linha</span>
             <input type="text" id="line" class="round" placeholder="Deixe em branco para adicionar na primeira linha">
         </div>
+
         <br><br>
         <div class="campo1">
         <br>
-            <span class="dbminputlabel">Texto</span>
-            <textarea id="text" class="round" rows="7" id="rows"></textarea>
+            <span class="dbminputlabel" name="xinelas">Texto</span><div style="float:right;margin-top:-5px"><dbm-checkbox id="addType2" onchange="glob.onChange2(this)" label="EVAL"></dbm-checkbox></div>
+            <div id="valor"><textarea id="text" class="round" rows="5" id="rows"></textarea></div>
+            <div style="display:none" id="valoreval"><textarea id="texteval" class="round" rows=" 5" name="is-eval" id="rows"></textarea></div>
         </div>
 
         <div class="campo3">
@@ -89,67 +122,86 @@ module.exports = {
             <input id="iffalseVal" class="round" type="text">
         </div>
 
-        <style>td{padding:5px;}xinspace{margin:5px 0px 0px 0px;display:block}</style>
+        <style>
+        td{padding:5px;}xinspace{margin:5px 0px 0px 0px;display:block}
+        .dbmmodsbr1{position:absolute;bottom:0px;border: 0px solid rgba(50,50,50,0.7);background:rgba(0,0,0,0.7);color:#999;padding:5px;left:0px;z-index:999999;cursor:pointer}
+        .dbmmodsbr2{position:absolute;bottom:0px;border: 0px solid rgba(50,50,50,0.7);background:rgba(0,0,0,0.7);color:#999;padding:5px;right:0px;z-index:999999;cursor:pointer}
+        </style>
       `;
     },
-  
+
     init() {
-        
+
         glob.onComparisonChanged = function (event) {
             if (event.value > "1") {
-              document.getElementById("iffalseContainer").style.display = null;
+                document.getElementById("iffalseContainer").style.display = null;
             } else {
-              document.getElementById("iffalseContainer").style.display = "none";
+                document.getElementById("iffalseContainer").style.display = "none";
             }
             if (event.value == "2") {
-            document.querySelector("[id='xinelas']").innerText = (`Número da ação`);
-          }
-          if (event.value == "3") {
-            document.querySelector("[id='xinelas']").innerText = (`Pular ações`);
-          }
-          if (event.value == "4") {
-            document.querySelector("[id='xinelas']").innerText = (`Nome da âncora`);
-          }
+                document.querySelector("[id='xinelas']").innerText = (`Número da ação`);
+            }
+            if (event.value == "3") {
+                document.querySelector("[id='xinelas']").innerText = (`Pular ações`);
+            }
+            if (event.value == "4") {
+                document.querySelector("[id='xinelas']").innerText = (`Nome da âncora`);
+            }
         }
-      
-          glob.onComparisonChanged(document.getElementById("iffalse"));
 
-        glob.change = function(event) {
+        glob.onComparisonChanged(document.getElementById("iffalse"));
+
+        glob.onChange2 = function (event) {
+            if (event.value == true) {
+                document.getElementById("valoreval").style.display = null;
+                document.getElementById("valor").style.display = "none";
+                document.querySelector("[name='xinelas']").innerText = (`EVAL`);
+            }
+            if (event.value == false) {
+                document.getElementById("valoreval").style.display = "none";
+                document.getElementById("valor").style.display = null;
+                document.querySelector("[name='xinelas']").innerText = (`Texto`);
+            }
+        };
+
+        glob.onChange2(document.getElementById("addType2"));
+
+        glob.change = function (event) {
             const value = parseInt(event.value);
 
-            if(value == 0) {
-                for(var i = 0; i < document.querySelectorAll(".campo1").length; i++) {
+            if (value == 0) {
+                for (var i = 0; i < document.querySelectorAll(".campo1").length; i++) {
                     document.querySelectorAll(".campo1")[i].style.display = "none";
                 }
                 document.querySelector(".campo3").style.display = "none";
                 document.querySelector(".campo4").style.display = "none";
-                document.getElementById("xinelasx").style.width = 100+"%";
-            } else if(value == 1 || value == 2) {
-                for(var i = 0; i < document.querySelectorAll(".campo1").length; i++) {
+                document.getElementById("xinelasx").style.width = 100 + "%";
+            } else if (value == 1 || value == 2) {
+                for (var i = 0; i < document.querySelectorAll(".campo1").length; i++) {
                     document.querySelectorAll(".campo1")[i].style.display = "block";
                 }
                 document.querySelector(".campo2").style.display = "none";
                 document.querySelector(".campo3").style.display = "none";
                 document.querySelector(".campo4").style.display = "none";
-                document.getElementById("xinelasx").style.width = 100+"%";
-            } else if(value == 3) {
-                for(var i = 0; i < document.querySelectorAll(".campo1").length; i++) {
+                document.getElementById("xinelasx").style.width = 100 + "%";
+            } else if (value == 3) {
+                for (var i = 0; i < document.querySelectorAll(".campo1").length; i++) {
                     document.querySelectorAll(".campo1")[i].style.display = "block";
                 }
                 document.querySelector(".campo2").style.display = "block";
                 document.querySelector(".campo3").style.display = "none";
                 document.querySelector(".campo4").style.display = "none";
-                document.getElementById("xinelasx").style.width = 50+"%";
-            } else if(value == 4) {
-                for(var i = 0; i < document.querySelectorAll(".campo1").length; i++) {
+                document.getElementById("xinelasx").style.width = 50 + "%";
+            } else if (value == 4) {
+                for (var i = 0; i < document.querySelectorAll(".campo1").length; i++) {
                     document.querySelectorAll(".campo1")[i].style.display = "none";
                 }
                 document.querySelector(".campo2").style.display = "none";
                 document.querySelector(".campo3").style.display = "none";
                 document.querySelector(".campo4").style.display = "none";
-                document.getElementById("xinelasx").style.width = 100+"%";
-            } else if(value == 5) {
-                for(var i = 0; i < document.querySelectorAll(".campo1").length; i++) {
+                document.getElementById("xinelasx").style.width = 100 + "%";
+            } else if (value == 5) {
+                for (var i = 0; i < document.querySelectorAll(".campo1").length; i++) {
                     document.querySelectorAll(".campo1")[i].style.display = "none";
                 }
                 document.querySelector(".campo2").style.display = "none";
@@ -157,31 +209,46 @@ module.exports = {
                 document.querySelector(".campo4").style.display = "block";
                 document.getElementById("loc").innerHTML = "Copiar para a pasta";
                 document.getElementById("nome2x").innerHTML = "Novo nome do arquivo";
-                document.getElementById("xinelasx").style.width = 100+"%";
-            } else if(value == 6) {
-            for(var i = 0; i < document.querySelectorAll(".campo1").length; i++) {
-                document.querySelectorAll(".campo1")[i].style.display = "none";
+                document.getElementById("xinelasx").style.width = 100 + "%";
+            } else if (value == 6) {
+                for (var i = 0; i < document.querySelectorAll(".campo1").length; i++) {
+                    document.querySelectorAll(".campo1")[i].style.display = "none";
+                }
+                document.querySelector(".campo2").style.display = "none";
+                document.querySelector(".campo3").style.display = "block";
+                document.querySelector(".campo4").style.display = "none";
+                document.getElementById("loc").innerHTML = "Mover para a pasta";
+                document.getElementById("xinelasx").style.width = 100 + "%";
+            } else if (value == 7) {
+                for (var i = 0; i < document.querySelectorAll(".campo1").length; i++) {
+                    document.querySelectorAll(".campo1")[i].style.display = "none";
+                }
+                document.querySelector(".campo2").style.display = "none";
+                document.querySelector(".campo3").style.display = "none";
+                document.querySelector(".campo4").style.display = "block";
+                document.getElementById("nome2x").innerHTML = "Renomear para";
+                document.getElementById("xinelasx").style.width = 100 + "%";
             }
-            document.querySelector(".campo2").style.display = "none";
-            document.querySelector(".campo3").style.display = "block";
-            document.querySelector(".campo4").style.display = "none";
-            document.getElementById("loc").innerHTML = "Mover para a pasta";
-            document.getElementById("xinelasx").style.width = 100+"%";
-        } else if(value == 7) {
-            for(var i = 0; i < document.querySelectorAll(".campo1").length; i++) {
-                document.querySelectorAll(".campo1")[i].style.display = "none";
-            }
-            document.querySelector(".campo2").style.display = "none";
-            document.querySelector(".campo3").style.display = "none";
-            document.querySelector(".campo4").style.display = "block";
-            document.getElementById("nome2x").innerHTML = "Renomear para";
-            document.getElementById("xinelasx").style.width = 100+"%";
-        }
         }
 
         glob.change(document.getElementById("function"));
+
+        const xinelaslinks = document.getElementsByClassName('xinelaslink');
+        for (let x = 0; x < xinelaslinks.length; x++) {
+          const xinelaslink = xinelaslinks[x];
+          const url = xinelaslink.getAttribute('data-url');
+          if (url) {
+           xinelaslink.setAttribute('title', url);
+           xinelaslink.addEventListener('click', (e) => {
+              e.stopImmediatePropagation();
+              console.log(`Launching URL: [${url}] in your default browser.`);
+              require('child_process').execSync(`start ${url}`);
+            });
+          }
+        }
+
     },
-  
+
     async action(cache) {
         const data = cache.actions[cache.index];
         const info = parseInt(data.function, 10);
@@ -189,7 +256,7 @@ module.exports = {
         path = require('path');
 
         try {
-            if(!this.evalMessage(data.path, cache)) {
+            if (!this.evalMessage(data.path, cache)) {
                 throw "File Control MOD: Insira a localização do arquivo!";
             }
 
@@ -199,19 +266,25 @@ module.exports = {
             const nome2 = this.evalMessage(data.nome2, cache);
             const format = this.evalMessage(data.format, cache);
 
-            if(!nome) {
+            if (!nome) {
                 throw "File Control MOD: Insira o nome do arquivo!";
             }
 
-            if(!format) {
+            if (!format) {
                 throw "File Control MOD: Insira o formato do arquivo!";
             }
 
             const filePath = path.join(diretorio, nome + "." + format);
 
-            const text = this.evalMessage(data.text, cache);
+            if (data.addType2 == true) {
+                text = this.evalMessage(data.texteval, cache)
+                text = this.eval(text, cache)
+                text = JSON.stringify(text)
+            } else {
+                text = this.evalMessage(data.text, cache)
+            }
 
-            switch(info) {
+            switch (info) {
                 case 1:
                     fs.writeFileSync(filePath, text);
                     break;
@@ -221,49 +294,49 @@ module.exports = {
                 case 3:
                     const insertLine = require('insert-line');
                     const line = this.evalMessage(data.line, cache) || 1;
-                    insertLine(filePath).content(text).at(line).then((err) => {});
+                    insertLine(filePath).content(text).at(line).then((err) => { });
                     break;
                 case 4:
                     fs.unlinkSync(filePath);
                     break;
                 case 5:
-                    if(!this.evalMessage(data.copy, cache)) {
+                    if (!this.evalMessage(data.copy, cache)) {
                         throw "File Control MOD: Insira a localização para onder copiar o arquivo!";
                     }
 
                     const diretorio2 = path.normalize(this.evalMessage(data.copy, cache));
-                    const filePath2 = path.join(diretorio2,nome2 + "." + format); 
+                    const filePath2 = path.join(diretorio2, nome2 + "." + format);
 
                     fs.copySync(filePath, filePath2);
                     break;
-                    case 6:
-                        if(!this.evalMessage(data.copy, cache)) {
-                            throw "File Control MOD: Insira a localização para onder copiar o arquivo!";
-                        }
+                case 6:
+                    if (!this.evalMessage(data.copy, cache)) {
+                        throw "File Control MOD: Insira a localização para onder copiar o arquivo!";
+                    }
 
-                        if (!fs.existsSync(data.copy)) {
-                            fs.mkdirSync(data.copy, {
-                                recursive: true
-                            });
-                        }
-                        var oldPath = this.evalMessage(data.path, cache)+this.evalMessage(data.nome, cache)+'.'+this.evalMessage(data.format, cache)
-                        var newPath = this.evalMessage(data.copy, cache)+this.evalMessage(data.nome, cache)+'.'+this.evalMessage(data.format, cache)
-                        fs.renameSync(oldPath, newPath)
-                        break;
-                        case 7:   
-                            var oldPath = this.evalMessage(data.path, cache)+this.evalMessage(data.nome, cache)+'.'+this.evalMessage(data.format, cache)
-                            var newPath = this.evalMessage(data.path, cache)+this.evalMessage(data.nome2, cache)+'.'+this.evalMessage(data.format, cache)
-                            fs.renameSync(oldPath, newPath)
-                            break;
+                    if (!fs.existsSync(data.copy)) {
+                        fs.mkdirSync(data.copy, {
+                            recursive: true
+                        });
+                    }
+                    var oldPath = this.evalMessage(data.path, cache) + this.evalMessage(data.nome, cache) + '.' + this.evalMessage(data.format, cache)
+                    var newPath = this.evalMessage(data.copy, cache) + this.evalMessage(data.nome, cache) + '.' + this.evalMessage(data.format, cache)
+                    fs.renameSync(oldPath, newPath)
+                    break;
+                case 7:
+                    var oldPath = this.evalMessage(data.path, cache) + this.evalMessage(data.nome, cache) + '.' + this.evalMessage(data.format, cache)
+                    var newPath = this.evalMessage(data.path, cache) + this.evalMessage(data.nome2, cache) + '.' + this.evalMessage(data.format, cache)
+                    fs.renameSync(oldPath, newPath)
+                    break;
             }
 
 
 
-        } catch(err) {
+        } catch (err) {
             this.executeResults(false, data, cache);
         }
         this.callNextAction(cache);
     },
-  
-    mod() {},
+
+    mod() { },
 };
