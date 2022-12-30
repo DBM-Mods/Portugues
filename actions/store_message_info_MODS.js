8ac2c666ba4fb0ee7827e03cb2b93da2e97f6767
XinXyla <43226244+XinXyla@users.noreply.github.com>
2022-12-30 13:42:47 -0300

    🆙 Atualização para a versão 1.7

--- actions/store_message_info_MODS.js
+++ actions/store_message_info_MODS.js
@@ -281,7 +281,7 @@ module.exports = {
   html(isEvent, data) {
     return `
     <div class="dbmmodsbr1 xinelaslink" data-url="https://github.com/DBM-Mods/Portugues/archive/refs/heads/main.zip">Atualizar</div>
-    <div class="dbmmodsbr2 xinelaslink" data-url="https://github.com/DBM-Mods/Portugues">Versão 1.6</div>
+    <div class="dbmmodsbr2 xinelaslink" data-url="https://github.com/DBM-Mods/Portugues">Versão 1.7</div>
 
     <div id="flutuador" style="padding:0px 0px 15px 0px">
       <table style="width:100%;"><tr>
@@ -508,7 +508,7 @@ module.exports = {
 
     if (!msg) {
       this.callNextAction(cache);
-      return this.callNextAction(cache);
+      return
     }
 
     const info = parseInt(data.info, 10);
