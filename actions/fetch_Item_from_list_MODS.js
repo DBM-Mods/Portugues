f4598c436a45b786dae4a2e407ac6334312e1ce4
XinXyla <43226244+XinXyla@users.noreply.github.com>
2023-01-18 12:26:31 -0300

    🆙 Atualização para a versão 0.7

--- actions/fetch_Item_from_list_MODS.js
+++ actions/fetch_Item_from_list_MODS.js
@@ -22,8 +22,15 @@ module.exports = {
       'Variável Servidor',
       'Variável Global',
     ];
-    const info = ['exatamente igual a', 'inclua', 'matches regex','menor que', 'menor ou igual a', 'maior que', 'maior ou igual a', 'comprimento maior que', 'comprimento menor que', 'comprimento igual a', 'começa com', 'termina com','possui acentuações','É um URL de imagem','É um URL','Não é um URL de imagem','Não é um URL','É um número','É um texto'];
-     return `Buscar ${info[parseInt(data.buscadoxin)]} "${data.item}" em "${data.varName}"`;
+    if(data.descriptionx == true){
+      desccor = data.descriptioncolor
+      } else {
+        desccor = 'none'
+      }
+    const info = ['exatamente igual a', 'inclua', 'matches regex','menor que', 'menor ou igual a', 'maior que', 'maior ou igual a', 'comprimento maior que', 'comprimento menor que', 'comprimento igual a', 'começa com', 'termina com','possui acentuações','É um URL de imagem','É um URL','Não é um URL de imagem','Não é um URL','É um número','É um texto','Que inclua ~ Ignorar Minúscula/Maiúscula'];
+    return data.description
+    ? `<font style="color:${desccor}">${data.description}</font>`
+    : `<font style="color:${desccor}">Buscar ${info[parseInt(data.buscadoxin)]} "${data.item}" em "${data.varName}"</font>`;
   },
 
   variableStorage(data, varType) {
@@ -32,14 +39,21 @@ module.exports = {
     return [data.varName2, 'Number'[prse2]];
   },
 
-  fields: ['list', 'varName', 'buscadoxin', 'item', 'storage', 'varName2','iffalse', 'iffalseVal'],
+  fields: ['list', 'varName', 'buscadoxin', 'item', 'storage', 'varName2','iffalse', 'iffalseVal', 'descriptioncolor', 'description', 'descriptionx'],
 
   html(isEvent, data) {
     return `
-    <div style="position:absolute;bottom:0px;border: 1px solid #222;background:#000;color:#999;padding:3px;right:0px;z-index:999999">Versão 0.6</div>
-    <div style="position:absolute;bottom:0px;border: 1px solid #222;background:#000;color:#999;padding:3px;left:0px;z-index:999999">dbmmods.com</div>
+    <div class="dbmmodsbr1 xinelaslink" data-url="https://github.com/DBM-Mods/Portugues/archive/refs/heads/main.zip">Atualizar</div>
+    <div class="dbmmodsbr2 xinelaslink" data-url="https://github.com/DBM-Mods/Portugues">Versão 0.7</div>
 
-    <div style="width: 100%; padding:10px 5px;height: calc(100vh - 170px);overflow:auto">
+    <div style="width: 100%; padding:5px 5px;height: calc(100vh - 160px);overflow:auto">
+
+    <div id="flutuador" style="padding:0px 0px 15px 0px">
+<table style="width:100%;"><tr>
+<td><span class="dbminputlabel">Descrição da Action</span><br><input type="text" class="round" id="description" placeholder="Deixe vazio para remover"></td>
+<td style="padding:0px 0px 0px 10px;width:70px"><div style="float:left;padding:0px 0px 0px 7px;margin-top:-5px"><dbm-checkbox id="descriptionx" label="Cor"></dbm-checkbox></div><br><input type="color" value="#ffffff" class="round" id="descriptioncolor"></td>
+</tr></table>
+</div>
 
 <div style="float: left; width: 35%;">
 <span class="dbminputlabel">Lista</span><br>
@@ -57,6 +71,7 @@ module.exports = {
 			<select id="buscadoxin" class="round" onchange="glob.onComparisonChanged2(this)">
 				<option value="0" selected>Exatamente igual a</option>
 				<option value="1">Que inclua</option>
+        <option value="19">Que inclua ~ Ignorar Minúscula/Maiúscula</option>
         <option value="2">Matches Regex</option>
         <option value="7">O comprimento é maior que</option>
         <option value="8">O comprimento é menor que</option>
@@ -77,7 +92,7 @@ module.exports = {
 			</select>
 		</div>
 <div style="padding-top: 8px;" id="xingoxyla">
-    <textarea id="item" rows="4" placeholder="Insira uma variável ou algum texto. Esses '' não são necessários!" style="width: 100%; font-family: monospace; white-space: nowrap;"></textarea>
+    <textarea id="item" rows="2" placeholder="Insira uma variável ou algum texto. Esses '' não são necessários!" style="width: 100%; font-family: monospace; white-space: nowrap;"></textarea>
 </div><br>
 
 
@@ -111,6 +126,10 @@ module.exports = {
 
 </div>
 <style>	
+
+.dbmmodsbr1{position:absolute;bottom:0px;border: 0px solid rgba(50,50,50,0.7);background:rgba(0,0,0,0.7);color:#999;padding:5px;left:0px;z-index:999999;cursor:pointer}
+.dbmmodsbr2{position:absolute;bottom:0px;border: 0px solid rgba(50,50,50,0.7);background:rgba(0,0,0,0.7);color:#999;padding:5px;right:0px;z-index:999999;cursor:pointer}
+
 table{width:100%}
 .col1{width:35%;padding:0px 10px 0px 0px}
 .col2{width:65%}
@@ -142,10 +161,10 @@ table{width:100%}
 
 
     glob.onComparisonChanged2 = function (event) {
-      if (event.value >= 12) {
-        document.getElementById("xingoxyla").style.display = "none";
-      } else {
+      if (event.value <= 11 || event.value == 19) {
         document.getElementById("xingoxyla").style.display = null;
+      } else {
+        document.getElementById("xingoxyla").style.display = "none";
       }
   }
 
@@ -163,6 +182,21 @@ table{width:100%}
     };
 
     glob.listChange(document.getElementById('list'), 'varNameContainer');
+
+    const xinelaslinks = document.getElementsByClassName('xinelaslink');
+    for (let x = 0; x < xinelaslinks.length; x++) {
+      const xinelaslink = xinelaslinks[x];
+      const url = xinelaslink.getAttribute('data-url');
+      if (url) {
+       xinelaslink.setAttribute('title', url);
+       xinelaslink.addEventListener('click', (e) => {
+          e.stopImmediatePropagation();
+          console.log(`Launching URL: [${url}] in your default browser.`);
+          require('child_process').execSync(`start ${url}`);
+        });
+      }
+    }
+
   },
 
     async action(cache) {
@@ -256,6 +290,9 @@ table{width:100%}
               result = [ix]}
           }}
         break;
+        case 19:
+          result = list.findIndex((i) => i.toLowerCase().includes(item.toLowerCase()));
+          break;
 		}
     
 
