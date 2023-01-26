2760b5a6ac3c9648b2c0a0e0100dea50ac9b23be
XinXyla <43226244+XinXyla@users.noreply.github.com>
2023-01-26 15:23:46 -0300

    🆙 Atualização para a versão 0.2

--- actions/timeout_member_MODS.js
+++ actions/timeout_member_MODS.js
@@ -1,43 +1,56 @@
 module.exports = {
-
   name: "Timeout Member MOD",
   section: "Member Control",
-
-  subtitle(data, presets) {
-    return `${presets.getMemberText(data.member, data.varName)}`;
-  },
-
   meta: {
     version: '2.1.6',
     preciseCheck: true,
-    author: '[Tempest - 321400509326032897]',
+    author: '[Tempest - 321400509326032897]<br>[XinXyla - 172782058396057602]',
     authorUrl: 'https://github.com/DBM-Mods/Portugues',
     downloadURL: 'https://github.com/DBM-Mods/Portugues/archive/refs/heads/main.zip',
   },
 
+  subtitle(data, presets) {
 
-  fields: ["opcao", "member", "varName", "tempo", "time", "reason"],
+    if (data.descriptionx == true) {
+      desccor = data.descriptioncolor
+    } else {
+      desccor = 'none'
+    }
+
+    return data.description
+      ? `<font style="color:${desccor}">${data.description}</font>`
+      : `<font style="color:${desccor}">${presets.getMemberText(data.member, data.varName)}</font>`
+  },
+
+  fields: ["opcao", "member", "varName", "tempo", "time", "reason", "iffalse", "iffalseVal", "descriptioncolor", "description", "descriptionx"],
 
   html(isEvent, data) {
     return `
-    <div style="position:absolute;bottom:0px;border: 1px solid #222;background:#000;color:#999;padding:3px;right:0px;z-index:999999">Versão 0.1</div>
-    <div style="position:absolute;bottom:0px;border: 1px solid #222;background:#000;color:#999;padding:3px;left:0px;z-index:999999">dbmmods.com</div>
+    <div class="dbmmodsbr1 xinelaslink" data-url="https://github.com/DBM-Mods/Portugues/archive/refs/heads/main.zip">Atualizar</div>
+    <div class="dbmmodsbr2 xinelaslink" data-url="https://github.com/DBM-Mods/Portugues">Versão 0.2</div>
+
+    <div style="width: 100%; padding:5px 5px;height: calc(100vh - 160px);overflow:auto">
+
+    <div id="flutuador" style="padding:0px 0px 15px 0px">
+<table style="width:100%;"><tr>
+<td><span class="dbminputlabel">Descrição da Action</span><br><input type="text" class="round" id="description" placeholder="Deixe vazio para remover"></td>
+<td style="padding:0px 0px 0px 10px;width:70px"><div style="float:left;padding:0px 0px 0px 7px;margin-top:-5px"><dbm-checkbox id="descriptionx" label="Cor"></dbm-checkbox></div><br><input type="color" value="#ffffff" class="round" id="descriptioncolor"></td>
+</tr></table>
+</div>
 
     <member-input dropdownLabel="Membro" selectId="member" variableContainerId="varNameContainer" variableInputId="varName"></member-input>
 
-    <br><br><br><br>
+    <br><br><br>
 
-    <div style="width: 30%;">
+    <div style="float: left; width: 33%">
       <span class="dbminputlabel">Opção</span>
       <select id="opcao" class="round" onchange="glob.change(this)">
         <option value="0" selected>Setar castigo</option>
         <option value="1">Remover castigo</option>
       </select>
     </div>
-
-    <br>
     
-    <div id="campo1" style="float: left; width: 45%;">
+    <div id="campo1" style="float: left; width: 34%;padding:0px 3px">
       <span class="dbminputlabel">Tempo</span><br>
       <select id="tempo" class="round">
         <option value="0" selected>Segundos</option>
@@ -46,19 +59,46 @@ module.exports = {
         <option value="3">Dias</option>
       </select>
     </div>
-    <div id="campo2" style="float: right; width: 50%;">
+    <div id="campo2" style="float: left; width: 33%">
       <span class="dbminputlabel">Quantidade</span><br>
       <input id="time" class="round" type="text">
     </div>
-
-    <div id="campo3">
-      <br><br><br>
-    </div>
-    
-    <div style="padding-top: 16px;">
+    <br><br><br>
+    <div>
       <span class="dbminputlabel">Razão</span><br>
-      <textarea id="reason" class="dbm_monospace" rows="5" placeholder="Insira a razão aqui..." style="white-space: nowrap; resize: none;"></textarea>
-    </div>`;
+      <textarea id="reason" class="dbm_monospace" rows="3" placeholder="Insira a razão aqui..." style="white-space: nowrap; resize: none;"></textarea>
+    </div>
+
+<br>
+
+    <div>
+<div style="float: left; width: 38%">
+<span class="dbminputlabel">Se ocorrer um erro</span><br>
+<select id="iffalse" class="round" onchange="glob.onComparisonChanged(this)">
+<option value="0" selected>Continuar ações</option>
+<option value="1">Parar sequência de ação</option>
+<option value="2">Ir para a ação</option>
+<option value="3">Pular as próximas ações</option>
+<option value="4">Ir para a âncora de ação</option>
+</select>
+</div>
+
+<div id="iffalseContainer" style="display: none; float: right; width: 60%;"><span id="xinelas" class="dbminputlabel">Para</span><br><input id="iffalseVal" class="round" name="actionxinxyla" type="text"></div>
+</div>
+
+</div>
+
+    
+    </div>
+
+<style>
+table{width:100%}
+.col1{width:38%;padding:0px 10px 0px 0px}
+.col2{width:60%}
+.dbmmodsbr1{position:absolute;bottom:0px;border: 0px solid rgba(50,50,50,0.7);background:rgba(0,0,0,0.7);color:#999;padding:5px;left:0px;z-index:999999;cursor:pointer}
+.dbmmodsbr2{position:absolute;bottom:0px;border: 0px solid rgba(50,50,50,0.7);background:rgba(0,0,0,0.7);color:#999;padding:5px;right:0px;z-index:999999;cursor:pointer}
+
+</style>`;
   },
 
 
@@ -67,20 +107,54 @@ module.exports = {
       if(event.value === "0") {
         document.getElementById("campo1").style.display = "block";
         document.getElementById("campo2").style.display = "block";
-        document.getElementById("campo3").style.display = "block";
       } else {
         document.getElementById("campo1").style.display = "none";
         document.getElementById("campo2").style.display = "none";
-        document.getElementById("campo3").style.display = "none";
       }
     }
 
     glob.change(document.getElementById("opcao"));
+
+    const xinelaslinks = document.getElementsByClassName('xinelaslink');
+    for (let x = 0; x < xinelaslinks.length; x++) {
+      const xinelaslink = xinelaslinks[x];
+      const url = xinelaslink.getAttribute('data-url');
+      if (url) {
+        xinelaslink.setAttribute('title', url);
+        xinelaslink.addEventListener('click', (e) => {
+          e.stopImmediatePropagation();
+          console.log(`Launching URL: [${url}] in your default browser.`);
+          require('child_process').execSync(`start ${url}`);
+        });
+      }
+    }
+
+    glob.onComparisonChanged = function (event) {
+      if (event.value > "1") {
+        document.getElementById("iffalseContainer").style.display = null;
+      } else {
+        document.getElementById("iffalseContainer").style.display = "none";
+      }
+      if (event.value == "2") {
+        document.querySelector("[id='xinelas']").innerText = (`Número da ação`);
+      }
+      if (event.value == "3") {
+        document.querySelector("[id='xinelas']").innerText = (`Pular ações`);
+      }
+      if (event.value == "4") {
+        document.querySelector("[id='xinelas']").innerText = (`Nome da âncora`);
+      }
+    }
+
+    glob.onComparisonChanged(document.getElementById("iffalse"));
+
   },
 
   async action(cache) {
     const data = cache.actions[cache.index];
-    const member = await this.getMemberFromData(data.member, data.varName, cache);
+    var find = this.evalMessage(data.varName, cache);
+    var memberfind = this.evalMessage(data.member, cache);
+    var member = await this.getMemberFromData(data.member, data.varName, cache);
     const tempo = parseInt(data.tempo, 10);
 
     let time = this.evalMessage(data.time, cache);
@@ -106,14 +180,28 @@ module.exports = {
     }
     const reason = this.evalMessage(data.reason, cache);
 
+    if(memberfind == "100" || memberfind == "101"){
+
+      const server = cache.server;
+      if (!server?.members) {
+        this.callNextAction(cache);
+        return;
+      }
+      if (server.memberCount !== server.members.cache.size) server.members.fetch();
+      const members = server.members.cache;
+
+      if(memberfind == "100"){member = members.find((m) => m.user?.username === find);}
+      if(memberfind == "101"){member = members.get(find)}
+    }
+
     if (Array.isArray(member)) {
       this.callListFunc(member, "disableCommunicationUntil", [time, reason])
         .then(() => this.callNextAction(cache))
-        .catch((err) => this.displayError(data, cache, err));
+        .catch((err) => this.displayError(data, cache, err) + this.executeResults(false, data, cache));
     } else if (member?.disableCommunicationUntil) {
       member.disableCommunicationUntil(time, reason)
         .then(() => this.callNextAction(cache))
-        .catch((err) => this.displayError(data, cache, err));
+        .catch((err) => this.displayError(data, cache, err) + this.executeResults(false, data, cache));
     } else {
       this.callNextAction(cache);
     }
