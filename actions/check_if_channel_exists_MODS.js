bc6ae9c2aa51c82f73ce6b72065a8cd1f8dc303d
Tempest <48209160+TempestDBM@users.noreply.github.com>
2023-03-02 21:45:45 -0300

    🆙 Atualização para a versão 0.4

--- actions/check_if_channel_exists_MODS.js
+++ actions/check_if_channel_exists_MODS.js
@@ -72,7 +72,7 @@ module.exports = {
 
   html(isEvent, data) {
     return `
-    <div style="position:absolute;bottom:0px;border: 1px solid #222;background:#000;color:#999;padding:3px;right:0px;z-index:999999">Versão 0.3</div>
+    <div style="position:absolute;bottom:0px;border: 1px solid #222;background:#000;color:#999;padding:3px;right:0px;z-index:999999">Versão 0.4</div>
     <div style="position:absolute;bottom:0px;border: 1px solid #222;background:#000;color:#999;padding:3px;left:0px;z-index:999999">dbmmods.com</div>
 
     <server-input dropdownLabel="Servidor" selectId="server" variableContainerId="varNameContainer" variableInputId="varName"></server-input>
@@ -85,6 +85,9 @@ module.exports = {
         <select id="info" class="round">
           <option value="0" selected>ID</option>
           <option value="1">Nome</option>
+          <option value="2">Tópico do Canal</option>
+          <option value="3">Posição do Canal</option>
+          <option value="4">ID da Categoria do Canal</option>
         </select>
       </div>
       <div style="float: right; width: 55%;">
@@ -126,10 +129,7 @@ module.exports = {
     const find = this.evalMessage(data.find, cache);
     const info = parseInt(data.info, 10);
 
-    if (!server?.channels) {
-      this.callNextAction(cache);
-      return;
-    }
+    if (!server?.channels) return this.callNextAction(cache);
 
     let channels;
 
@@ -141,15 +141,23 @@ module.exports = {
 
     switch(info) {
         case 0:
-          result = Boolean(channels.get(find));
+          result = channels.get(find);
           break;
         case 1:
-          result = Boolean(channels.find((c) => c.name === find));
+          result = channels.find((c) => c.name === find);
+          break;
+        case 2:
+          result = channels.find((c) => c.topic === find);
+          break;
+        case 3:
+          result = channels.find((c) => c.rawPosition === parseInt(find));
+          break;
+        case 4:
+          result = channels.find((c) => c.parentId === find);
           break;
     }
 
-
-    this.executeResults(result, data?.branch ?? data, cache);
+    this.executeResults(Boolean(result), data?.branch ?? data, cache);
   },
 
   //---------------------------------------------------------------------
