a1113a7c9fbd24d8073325201e962a65f224b594
XinXyla <43226244+XinXyla@users.noreply.github.com>
2023-03-29 18:56:48 -0300

    🆙 Atualize o modulo discord.js@13.14.0

--- bot.js
+++ bot.js
@@ -9,7 +9,7 @@ DBM.version = "2.1.6";
 
 const DiscordJS = (DBM.DiscordJS = require("discord.js"));
 
-const requiredDjsVersion = "13.7.0";
+const requiredDjsVersion = "13.14.0";
 if (DiscordJS.version < requiredDjsVersion) {
   console.log(
     `This version of Discord Bot Maker requires discord.js ${requiredDjsVersion}+.\nPlease use "Project > Module Manager" and "Project > Reinstall Node Modules" to update to discord.js ${requiredDjsVersion}.\n`,
