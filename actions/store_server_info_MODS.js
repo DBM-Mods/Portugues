bf041efe4bd96aa0f9001cc53ff96eeed48080e3
XinXyla <43226244+XinXyla@users.noreply.github.com>
2023-03-29 22:01:28 -0300

    🆙 Atualização para a versão 1.7

--- actions/store_server_info_MODS.js
+++ actions/store_server_info_MODS.js
@@ -351,7 +351,7 @@ module.exports = {
   html(isEvent, data) {
     return `
     <div class="dbmmodsbr1 xinelaslink" data-url="https://github.com/DBM-Mods/Portugues/archive/refs/heads/main.zip">Atualizar</div>
-    <div class="dbmmodsbr2 xinelaslink" data-url="https://github.com/DBM-Mods/Portugues">Versão 1.6</div>
+    <div class="dbmmodsbr2 xinelaslink" data-url="https://github.com/DBM-Mods/Portugues">Versão 1.7</div>
 
     <style>
       .dbmmodsbr1 {
@@ -598,7 +598,7 @@ module.exports = {
         result = await targetServer.fetchOwner();
         break;
       case 12:
-        result = targetServer.me;
+        result = targetServer.members.me;
         break;
       case 13:
         result = [...targetServer.channels.cache.values()];
