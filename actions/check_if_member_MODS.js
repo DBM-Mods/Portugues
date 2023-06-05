7c9ab107fb4cece6a60211c0683aa1410d835229
XinXyla <43226244+XinXyla@users.noreply.github.com>
2023-06-05 02:15:14 -0300

    🆙 Atualizado para a versão 0.7

--- actions/check_if_member_MODS.js
+++ actions/check_if_member_MODS.js
@@ -32,7 +32,10 @@ module.exports = {
       "Está no canal AFK?",
       "Impulsionou o servidor?",
       "É um usuário?",
-      "Está no servidor atual?"
+      "Está no servidor atual?",
+      "Foi banido do servidor atual?",
+      "Possui convites no servidor atual?",
+      "Está no tópico/postagem?"
     ];
 
     return data.description
@@ -40,12 +43,12 @@ module.exports = {
       : `<font style="color:${desccor}">${info[parseInt(data.info, 10)]} > ${presets.getConditionsText(data)}</font>`
   },
 
-  fields: ['member', 'varName', 'info', 'varName2', "comparison", "branch", "descriptioncolor", "description", "descriptionx"],
+  fields: ['member', 'varName', 'info', 'varName2', "comparison", "branch", "thread", "threadVarName", "descriptioncolor", "description", "descriptionx"],
 
   html(isEvent, data) {
     return `
     <div class="dbmmodsbr1 xinelaslink" data-url="https://github.com/DBM-Mods/Portugues/archive/refs/heads/main.zip">Atualizar</div>
-    <div class="dbmmodsbr2 xinelaslink" data-url="https://github.com/DBM-Mods/Portugues">Versão 0.6</div>
+    <div class="dbmmodsbr2 xinelaslink" data-url="https://github.com/DBM-Mods/Portugues">Versão 0.7</div>
 
     <div style="width: 100%; padding:5px 5px;height: calc(100vh - 160px);overflow:auto">
 
@@ -62,7 +65,7 @@ module.exports = {
 <div>
   <div style="float: left; width: 100%;padding-top:8px">
   <span class="dbminputlabel">Verifique se o membro</span><br>
-    <select id="info" class="round">
+    <select id="info" class="round" onchange="glob.change(this)">
       <option value="0" selected>É um Bot?</option>
       <option value="13">É um usuário?</option>
       <option value="1">Pode ser banido?</option>
@@ -77,15 +80,26 @@ module.exports = {
       <option value="11">Está no canal AFK?</option>
       <option value="12">Impulsionou o servidor?</option>
       <option value="14">Está no servidor atual?</option>
+      <option value="15">Foi banido do servidor atual?</option>
+      <option value="16">Possui convites no servidor atual?</option>
+      <option value="17">Está no tópico/postagem?</option>
     </select>
   </div>
-</div><br><br><br><br>
-<hr class="subtlebar"><br>
+</div><br><br><br>
+
+<div id="thrxin" style="overflow:hidden">
+<br>
+<thread-channel-input dropdownLabel="Canal" selectId="thread" variableContainerId="varNameContainer2" variableInputId="threadVarName"></thread-channel-input>
+<br><br>
+</div>
+<xinspace>
+
+<hr class="subtlebar">
 <conditional-input id="branch" style="padding-top: 8px;"></conditional-input>
 
 </div>
 <style>
-
+xinspace{padding:10px 0px 0px 0px;display:block}
 .dbmmodsbr1{position:absolute;bottom:0px;border: 0px solid rgba(50,50,50,0.7);background:rgba(0,0,0,0.7);color:#999;padding:5px;left:0px;z-index:999999;cursor:pointer}
 .dbmmodsbr2{position:absolute;bottom:0px;border: 0px solid rgba(50,50,50,0.7);background:rgba(0,0,0,0.7);color:#999;padding:5px;right:0px;z-index:999999;cursor:pointer}
 
@@ -99,6 +113,16 @@ module.exports = {
 
   init() {
     const { glob, document } = this;
+
+    glob.change = function (event) {
+      if (event.value == "17") {
+        document.getElementById("thrxin").style.display = null;
+      } else {
+        document.getElementById("thrxin").style.display = "none";
+      }
+    }
+
+    glob.change(document.getElementById("info"));
 
     const xinelaslinks = document.getElementsByClassName('xinelaslink');
     for (let x = 0; x < xinelaslinks.length; x++) {
@@ -198,6 +222,27 @@ module.exports = {
 
           result = Boolean(members.get(member.id));
         }
+        break;
+      case 15:
+        const bans = await targetServer.bans.fetch();
+        banidos = [...bans.values()];
+        banidos = banidos.map(v => v.user)
+        banido = banidos.findIndex((item) => item.id == member.id)
+        if (banido == -1) { result = false } else { result = true }
+        break;
+      case 16:
+        const invites = await targetServer.invites.fetch();
+        convites = [...invites.values()];
+        convites = convites.map(v => v.inviter)
+        convite = convites.findIndex((item) => item.id == member.id)
+        if (convite == -1) { result = false } else { result = true }
+        break;
+      case 17:
+        const targetChannel = await this.getChannelFromData(data.thread, data.threadVarName, cache);
+        const members = await targetChannel.members.fetch();
+        const ids = Array.from(members.keys());
+        id = ids.findIndex((item) => item == member.id)
+        if (id == -1) { result = false } else { result = true }
         break;
       default:
         console.log('Verifique sua ação "Verifique se o membro"! Há algo errado...');
