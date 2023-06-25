3ec60562a5db6fbb6caea9e97c0cbe0368608099
XinXyla <43226244+XinXyla@users.noreply.github.com>
2023-06-25 14:28:17 -0300

    🆙 Atualizado para a versão 0.2

--- actions/canvas_crop_image_MODS.js
+++ actions/canvas_crop_image_MODS.js
@@ -4,22 +4,42 @@ module.exports = {
   meta: {
     version: '2.1.7',
     preciseCheck: true,
-    author: '[XinXyla - 172782058396057602]',
+    author: '[xinxyla - 172782058396057602]',
     authorUrl: 'https://github.com/DBM-Mods/Portugues',
     downloadURL: 'https://github.com/DBM-Mods/Portugues/archive/refs/heads/main.zip',
   },
 
-  subtitle (data) {
+  subtitle(data) {
+
+    if (data.descriptionx == true) {
+      desccor = data.descriptioncolor
+    } else {
+      desccor = 'none'
+    }
+
     const storeTypes = ['', 'Variavel Temporaria', 'Variavel Servidor', 'Variavel Global']
-    return `${storeTypes[parseInt(data.storage)]} (${data.varName})`
+
+    return data.description
+      ? `<font style="color:${desccor}">${data.description}</font>`
+      : `<font style="color:${desccor}">${storeTypes[parseInt(data.storage)]} (${data.varName})</font>`
   },
 
-  fields: ['storage', 'varName', 'align', 'align2', 'width', 'height', 'positionx', 'positiony'],
+  fields: ['storage', 'varName', 'align', 'align2', 'width', 'height', 'positionx', 'positiony', 'descriptioncolor', 'description', 'descriptionx'],
 
   html (isEvent, data) {
     return `
-    <div style="position:absolute;bottom:0px;border: 1px solid #222;background:#000;color:#999;padding:3px;right:0px;z-index:999999">Versão 0.1</div>
-    <div style="position:absolute;bottom:0px;border: 1px solid #222;background:#000;color:#999;padding:3px;left:0px;z-index:999999">dbmmods.com</div>
+    <div class="dbmmodsbr1 xinelaslink" data-url="https://github.com/DBM-Mods/Portugues/archive/refs/heads/main.zip">Atualizar</div>
+    <div class="dbmmodsbr2 xinelaslink" data-url="https://github.com/DBM-Mods/Portugues">Versão 0.2</div>
+
+    <div style="width: 100%; padding:5px 5px;height: calc(100vh - 160px);overflow:auto">
+
+    <div id="flutuador" style="padding:0px 0px 15px 0px">
+<table style="width:100%;"><tr>
+<td><span class="dbminputlabel">Descrição da Action</span><br><input type="text" class="round" id="description" placeholder="Deixe vazio para remover"></td>
+<td style="padding:0px 0px 0px 10px;width:70px"><div style="float:left;padding:0px 0px 0px 7px;margin-top:-5px"><dbm-checkbox id="descriptionx" label="Cor"></dbm-checkbox></div><br><input type="color" value="#ffffff" class="round" id="descriptioncolor"></td>
+</tr></table>
+</div>
+
 <div>
   <div style="float: left; width: 45%;">
   <span class="dbminputlabel">Imagem Canvas</span><br>
@@ -27,21 +47,24 @@ module.exports = {
       ${data.variables[1]}
     </select><br>
   </div>
+
   <div id="varNameContainer" style="float: right; width: 50%;">
   <span class="dbminputlabel">Nome da Variavel</span><br>
     <input id="varName" class="round" type="text" list="variableList"><br>
   </div>
 </div><br><br><br>
+
 <div>
-  <div style="float: left; width: 50%;padding:5px">
+  <div style="float: left; width: 50%;padding:0px 5px 0px 0px">
   <span class="dbminputlabel">Cortar largura (px ou %)</span><br>
     <input id="width" class="round" type="text" value="100%"><br>
   </div>
-  <div style="float: right; width: 50%;padding:5px">
+  <div style="float: right; width: 50%;padding:0px 0px 0px 5px">
   <span class="dbminputlabel">Cortar altura (px ou %)</span><br>
     <input id="height" class="round" type="text" value="100%"><br>
   </div>
 </div><br><br><br>
+
   <div style="float: left; width: 100%;">
   <span class="dbminputlabel">Alinhamento</span><br>
     <select id="align" class="round" onchange="glob.onChange0(this)">
@@ -49,7 +72,7 @@ module.exports = {
     <option value="1">Centro superior</option>
     <option value="2">Canto superior direito</option>
     <option value="3">Meio esquerdo</option>
-    <option value="4">Centro Central</option>
+    <option value="4">Centro</option>
     <option value="5">Meio direito</option>
     <option value="6">Inferior esquerdo</option>
     <option value="7">Centro inferior</option>
@@ -57,6 +80,7 @@ module.exports = {
     <option value="9">Posição específica</option>
     </select><br>
   </div>
+
   <div id="specific" style="display: none; float: left; width: 100%;">
   <span class="dbminputlabel">Alinhamento customizado</span><br>
     <select id="align2" class="round">
@@ -64,14 +88,15 @@ module.exports = {
     <option value="1">Centro superior</option>
     <option value="2">Canto superior direito</option>
     <option value="3">Meio esquerdo</option>
-    <option value="4">Centro Central</option>
+    <option value="4">Centro</option>
     <option value="5">Meio direito</option>
     <option value="6">Inferior esquerdo</option>
     <option value="7">Centro inferior</option>
     <option value="8">Inferior direito</option>
     </select><br>
   </div>
-</div><br><br>
+
+  
 <div id="position" style="display: none">
   <div style="float: left; width: 50%;padding:5px">
   <span class="dbminputlabel">Posição X</span><br>
@@ -81,7 +106,16 @@ module.exports = {
   <span class="dbminputlabel">Posição Y</span><br>
     <input id="positiony" class="round" type="text" value="0"><br>
   </div>
-</div>`
+</div>
+
+</div>
+
+<style>
+
+.dbmmodsbr1{position:absolute;bottom:0px;border: 0px solid rgba(50,50,50,0.7);background:rgba(0,0,0,0.7);color:#999;padding:5px;left:0px;z-index:999999;cursor:pointer}
+.dbmmodsbr2{position:absolute;bottom:0px;border: 0px solid rgba(50,50,50,0.7);background:rgba(0,0,0,0.7);color:#999;padding:5px;right:0px;z-index:999999;cursor:pointer}
+
+</style>`
   },
 
   init () {
@@ -102,6 +136,21 @@ module.exports = {
 
     glob.refreshVariableList(document.getElementById('storage'))
     glob.onChange0(document.getElementById('align'))
+
+    const xinelaslinks = document.getElementsByClassName('xinelaslink');
+    for (let x = 0; x < xinelaslinks.length; x++) {
+      const xinelaslink = xinelaslinks[x];
+      const url = xinelaslink.getAttribute('data-url');
+      if (url) {
+        xinelaslink.setAttribute('title', url);
+        xinelaslink.addEventListener('click', (e) => {
+          e.stopImmediatePropagation();
+          console.log(`Launching URL: [${url}] in your default browser.`);
+          require('child_process').execSync(`start ${url}`);
+        });
+      }
+    }
+
   },
 
   action (cache) {
