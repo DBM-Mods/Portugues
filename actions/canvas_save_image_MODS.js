cbecc47dccc70b8f937860dde3b3ec3dbbebb5dd
XinXyla <43226244+XinXyla@users.noreply.github.com>
2024-01-18 14:10:33 -0300

    🆙 Atualizado para a versão 0.2

--- actions/canvas_save_image_MODS.js
+++ actions/canvas_save_image_MODS.js
@@ -9,22 +9,42 @@ module.exports = {
     downloadURL: 'https://github.com/DBM-Mods/Portugues/archive/refs/heads/main.zip',
   },
 
-  subtitle (data) {
-    return `Salvar em "${data.Path}"`
+  subtitle(data, presets) {
+
+
+    if (data.descriptionx == true) {
+      desccor = data.descriptioncolor
+    } else {
+      desccor = 'none'
+    }
+
+    return data.description
+      ? `<font style="color:${desccor}">${data.description}</font>`
+      : `<font style="color:${desccor}">Salvar em "${data.Path}${data.fileName}"</font>`
   },
 
-  variableStorage (data, varType) {
+  variableStorage(data, varType) {
     const type = parseInt(data.storage2)
     if (type !== varType) return
     return ([data.varName2, 'Image URL'])
   },
 
-  fields: ['storage', 'varName', 'Path', 'storage2', 'varName2'],
+  fields: ['storage', 'varName', 'Path', 'fileName', 'storage2', 'varName2', 'descriptioncolor', 'description', 'descriptionx'],
 
-  html (isEvent, data) {
+  html(isEvent, data) {
     return `
-    <div style="position:absolute;bottom:0px;border: 1px solid #222;background:#000;color:#999;padding:3px;right:0px;z-index:999999">Versão 0.1</div>
-    <div style="position:absolute;bottom:0px;border: 1px solid #222;background:#000;color:#999;padding:3px;left:0px;z-index:999999">dbmmods.com</div>
+    <div class="dbmmodsbr1 xinelaslink" data-url="https://github.com/DBM-Mods/Portugues/archive/refs/heads/main.zip">Atualizar</div>
+    <div class="dbmmodsbr2 xinelaslink" data-url="https://github.com/DBM-Mods/Portugues">Versão 0.2</div>
+
+    <div style="width: 100%; padding:5px 5px;height: calc(100vh - 160px);overflow:auto">
+
+    <div id="flutuador" style="padding:0px 0px 15px 0px">
+<table style="width:100%;"><tr>
+<td><span class="dbminputlabel">Descrição da Action</span><br><input type="text" class="round" id="description" placeholder="Deixe vazio para remover"></td>
+<td style="padding:0px 0px 0px 10px;width:70px"><div style="float:left;padding:0px 0px 0px 7px;margin-top:-5px"><dbm-checkbox id="descriptionx" label="Cor"></dbm-checkbox></div><br><input type="color" value="#ffffff" class="round" id="descriptioncolor"></td>
+</tr></table>
+</div>
+
 <div>
   <div style="float: left; width: 40%;">
   <span class="dbminputlabel">Imagem Canvas</span><br>
@@ -39,10 +59,13 @@ module.exports = {
 </div><br><br><br>
 <div>
   <div style="float: left; width: 100%;">
-  <span class="dbminputlabel">Caminho (Salvar no Local)</span><br>
-    <input id="Path" class="round" type="text" placeholder="resources/output.png"><br>
+  <span class="dbminputlabel">Pasta (Salvar no Local)</span><br>
+    <input id="Path" class="round" value="./resources/" type="text" placeholder="./resources/"><br>
   </div>
 </div><br><br>
+<span class="dbminputlabel">Nome do arquivo + Formato</span><br>
+<input id="fileName" class="round" value="imagem.png" type="text" placeholder="imagem.png">
+<br>
 <div>
   <div style="float: left; width: 40%;">
   <span class="dbminputlabel">Armazenar em</span><br>
@@ -54,26 +77,48 @@ module.exports = {
   <span class="dbminputlabel">Nome da Variavel</span><br>
     <input id="varName2" class="round" type="text"><br>
   </div>
-</div>`
+</div>
+
+</div>
+
+<style>
+.dbmmodsbr1{position:absolute;bottom:0px;border: 0px solid rgba(50,50,50,0.7);background:rgba(0,0,0,0.7);color:#999;padding:5px;left:0px;z-index:999999;cursor:pointer}
+.dbmmodsbr2{position:absolute;bottom:0px;border: 0px solid rgba(50,50,50,0.7);background:rgba(0,0,0,0.7);color:#999;padding:5px;right:0px;z-index:999999;cursor:pointer}
+.xinelaslink{margin-top:-4px}
+.xinelaslink:hover{opacity:0.8 !important}
+</style>`
   },
 
-  init () {
+  init() {
     const { document, glob } = this
+
+    const xinelaslinks = document.getElementsByClassName('xinelaslink');
+    for (let x = 0; x < xinelaslinks.length; x++) {
+      const xinelaslink = xinelaslinks[x];
+      const url = xinelaslink.getAttribute('data-url');
+      if (url) {
+        xinelaslink.setAttribute('title', url);
+        xinelaslink.addEventListener('click', (e) => {
+          e.stopImmediatePropagation();
+          console.log(`Launching URL: [${url}] in your default browser.`);
+          require('child_process').execSync(`start ${url}`);
+        });
+      }
+    }
 
     glob.onComparisonChanged = function (event) {
       if (event.value == 0) {
         document.getElementById("containerxin").style.display = "none";
-      } else
-      {document.getElementById("containerxin").style.display = "block";}
+      } else { document.getElementById("containerxin").style.display = "block"; }
     };
 
     glob.onComparisonChanged(document.getElementById("storage2"));
     glob.refreshVariableList(document.getElementById('storage'))
   },
 
-  action (cache) {
+  action(cache) {
     const data = cache.actions[cache.index]
-    const fs = require('fs')
+    fs = require('fs-extra')
     const Canvas = require('canvas')
     const storage = parseInt(data.storage)
     const varName = this.evalMessage(data.varName, cache)
@@ -88,7 +133,17 @@ module.exports = {
     const ctx = canvas.getContext('2d')
     ctx.drawImage(image, 0, 0, image.width, image.height)
     const buffer = canvas.toBuffer()
-    const Path = this.evalMessage(data.Path, cache)
+    Path = this.evalMessage(data.Path, cache)
+    const fileName = this.evalMessage(data.fileName, cache)
+  
+    if (fileName) {
+      if (!fs.existsSync(Path) && Path) {
+        fs.mkdirSync(Path, {
+          recursive: true
+        });
+      }
+      Path += fileName
+    }
     if (Path) {
       fs.writeFileSync(Path, buffer)
       const varName2 = this.evalMessage(data.varName2, cache)
@@ -100,5 +155,5 @@ module.exports = {
     this.callNextAction(cache)
   },
 
-  mod () {}
+  mod() { }
 }
